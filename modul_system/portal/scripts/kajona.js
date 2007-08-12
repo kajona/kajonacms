560fd6a5a13fd212efc8f087491333daa1b652e2
sidler <sidler@mulchprod.de>
2007-08-12 20:37:31 +0000

    Moved portal-kajona.js from pages to system Added portions of Yahoos Interface Library in Version 2.3.0 to the portal. Debug-Scripts still included, before release 3.1, the min-files should be deployed as default

--- modul_system/portal/scripts/kajona.js
+++ modul_system/portal/scripts/kajona.js
@@ -1,7 +1,7 @@
 //   (c) 2004-2006 by MulchProductions, www.mulchprod.de
 //   (c) 2007 by Kajona, www.kajona.de
 //       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt
-//       $Id$
+//       $Id: kajona.js 1647 2007-08-10 19:40:40Z sidler $
 
 
 function portalEditorHover(elementSysId) {
