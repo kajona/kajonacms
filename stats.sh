9ceed2b7e2e8c2d30570cf8afe3520fa0c0046c6
sidler <sidler@mulchprod.de>
2007-09-09 14:47:47 +0000

    Stats exclude non-kajona sources

--- stats.sh
+++ stats.sh
@@ -8,7 +8,7 @@
 regularFile() 
 { #return 0: true return 1: falsch
 	# $1 = filename
-	testpattern=".gif .png .jpg .sh~ .php~ .ttf .csv .sql .gz"
+	testpattern=".gif .png .jpg .sh~ .php~ .ttf .csv .sql .gz .log .ico .ini"
 	for check in $testpattern
 	do
 		grepCheck=$(echo $i | grep $check)
@@ -29,28 +29,55 @@ checkLoc()
 	for i in `ls "$1"`
 	do
 		#echo $i
-		if [ $i != "stats.sh" ] && [ $i != "copy.log" ] && [ $i != "copy.php" ] && [ $i != "local_backup.sh" ]
+		if [ $i != "stats.sh" ] && [ $i != "copy.log" ] && [ $i != "copy_down.log" ] && [ $i != "copy.php" ] && [ $i != "local_backup.sh" ]
 		then
 			if [ -d "$1$i" ] && [ "$i" != ".svn" ]
 			then
 				checkLoc "$1$i/"
+				
 			fi
-			if [ -f "$1$i" ] && regularFile "$i"
+			if [ -f "$1$i" ]
 			then
-				temp=$(cat "$1$i" | wc -l)
-				nrLOC=$(($nrLOC + $temp))
+				if regularFile "$i"
+				then
+					temp=$(cat "$1$i" | wc -l)
+					nrLOC=$((nrLOC + $temp))
+					nrLOCFiles=$((nrLOCFiles + 1))
+					if checkKajonaLoc "$1$i"
+					then
+						nrKajonaLOC=$((nrKajonaLOC + $temp))
+						nrKajonaLOCFiles=$((nrKajonaLOCFiles + 1))
+					fi
+				fi
+				nrFilesAnalyzed=$((nrFilesAnalyzed + 1))
+				echo -n -e "\r... analyzed $nrFilesAnalyzed of $nrFiles files ..."
 			fi
 		fi
 	done
 }
 
+checkKajonaLoc()
+{
+	testpattern="yui fckeditor jscalendar system/fonts jpgraph ip2country"
+	for check in $testpattern
+	do
+		grepCheck=$(echo $1 | grep $check)
+		anzahlCount=$(echo $grepCheck | wc -m )
+		if [ $anzahlCount != "1" ]
+		then
+			#echo "excluding " $1 "out of kajona LOC"
+			return 1
+		fi
+	done
+	#echo "kLOC: $1"
+	return 0	
+}
 
 checkNrFiles() 
 {		
 	for i in `ls "$1"`
 	do
-		#echo $i
-		if [ $i != "stats.sh" ] && [ $i != "copy.log" ] && [ $i != "copy.php" ] && [ $i != "local_backup.sh" ]
+		if [ $i != "stats.sh" ] && [ $i != "copy.log" ] && [ $i != "copy_down.log" ] && [ $i != "copy.php" ] && [ $i != "local_backup.sh" ]
 		then
 			if [ -d "$1$i" ] && [ "$i" != ".svn" ]
 			then
@@ -58,7 +85,7 @@ checkNrFiles()
 			fi
 			if [ -f "$1$i" ]
 			then
-				nrFiles=$(($nrFiles + 1))
+				nrFiles=$((nrFiles + 1))
 			fi
 		fi
 	done
@@ -66,16 +93,28 @@ checkNrFiles()
 
 
 
-echo "Analyzing files...."
+echo "Analyzing files..."
 
 nrFiles=0
+nrFilesAnalyzed=0
 nrLOC=0
+nrKajonaLOC=0
+nrLOCFiles=0
+nrKajonaLOCFiles=0
 echo "... counting files ..."
 checkNrFiles "$PWD/"
 echo "... counting LOC ..."
 checkLoc "$PWD/"
+echo ""
 echo "... finished"
 echo ""
 echo ""
-echo "Number of files : $nrFiles"
-echo "Lines of code   : $nrLOC"
+echo "Number of files       : $nrFiles"
+echo ""
+echo "Total lines of code   : $nrLOC"
+echo "Files analyzed        : $nrLOCFiles"
+echo ""
+echo "Kajona lines of code  : $nrKajonaLOC"
+echo "Files analyzed        : $nrKajonaLOCFiles"
+echo ""
+echo ""
