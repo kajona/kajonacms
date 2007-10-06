56807caaf7e60880e9cde600de6c0b759e593c34
sidler <sidler@mulchprod.de>
2007-10-06 18:14:16 +0000

    added quickhelp texts for the dashboard

--- modul_system/texte/admin/modul_dashboard/texte_dashboard_de.php
+++ modul_system/texte/admin/modul_dashboard/texte_dashboard_de.php
@@ -35,4 +35,10 @@ $text["column1"]                    = "Spalte 1";
 $text["column2"]                    = "Spalte 2";
 $text["column3"]                    = "Spalte 3";
 
+$text["quickhelp_list"]             = "Das Dashboard dient als Ablage für Widgets jeglicher Art. Widgets stellen Miniprogramme dar, welche Informationen
+									   ausgeben. Diese Widgets können auf dem Dashboard sortiert, editiert und gelöscht werden. Das Hinzufügen neuer
+									   Widgets erfolgt über die Aktion \"Widget hinzufügen\". ";
+$text["quickhelp_addWidgetToDashboard"] = "Das Hinzufügen neuer Widgets zum Dashboard ist ein zweistufiger Prozess. Im ersten Schritt kann das hinzuzufügende
+									   Widget samt der Zielspalte bestimmt werden, im zweiten Schritt kann das anzulegende Widget konfiguriert werden.";
+
 ?>
