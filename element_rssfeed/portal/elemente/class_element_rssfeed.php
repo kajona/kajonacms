12904b2ec7398d00a8d43fe8a7d7465a3a1fa083
sidler <sidler@mulchprod.de>
2007-11-11 14:51:14 +0000

    rssfeed element is able to parse atom feeds

--- element_rssfeed/portal/elemente/class_element_rssfeed.php
+++ element_rssfeed/portal/elemente/class_element_rssfeed.php
@@ -66,24 +66,54 @@ class class_element_rssfeed extends class_element_portal implements interface_po
 		    $arrFeed = $objXmlparser->xmlToArray();
 		    
 		    if(count($arrFeed) >= 1) {
-    		    $arrTemplate["feed_title"] = $arrFeed["rss"][0]["channel"][0]["title"][0]["value"];
-    		    $arrTemplate["feed_link"] = $arrFeed["rss"][0]["channel"][0]["link"][0]["value"];
-    		    $arrTemplate["feed_description"] = $arrFeed["rss"][0]["channel"][0]["description"][0]["value"];
-    		    $intCounter = 0;
-    		    foreach ($arrFeed["rss"][0]["channel"][0]["item"] as $arrOneItem) {
-    		        
-    		        $arrMessage = array();
-    		        $arrMessage["post_date"] = (isset($arrOneItem["pubDate"][0]["value"]) ? $arrOneItem["pubDate"][0]["value"] : "");
-    		        $arrMessage["post_title"] = (isset($arrOneItem["title"][0]["value"]) ? $arrOneItem["title"][0]["value"] : "");
-    		        $arrMessage["post_description"] = (isset($arrOneItem["description"][0]["value"]) ? $arrOneItem["description"][0]["value"] : "");
-    		        $arrMessage["post_link"] = (isset($arrOneItem["link"][0]["value"]) ? $arrOneItem["link"][0]["value"] : "");
-        
-    	   	        $strContent .= $this->objTemplate->fillTemplate($arrMessage, $strPostTemplateID);
-
-    	   	        if(++$intCounter >= $this->arrElementData["int1"])
-    	   	           break;
-    		    
-    		    }
+		        
+		        //rss feed
+		        if(isset($arrFeed["rss"])) {
+		        
+        		    $arrTemplate["feed_title"] = $arrFeed["rss"][0]["channel"][0]["title"][0]["value"];
+        		    $arrTemplate["feed_link"] = $arrFeed["rss"][0]["channel"][0]["link"][0]["value"];
+        		    $arrTemplate["feed_description"] = $arrFeed["rss"][0]["channel"][0]["description"][0]["value"];
+        		    $intCounter = 0;
+        		    foreach ($arrFeed["rss"][0]["channel"][0]["item"] as $arrOneItem) {
+        		        
+        		        $arrMessage = array();
+        		        $arrMessage["post_date"] = (isset($arrOneItem["pubDate"][0]["value"]) ? $arrOneItem["pubDate"][0]["value"] : "");
+        		        $arrMessage["post_title"] = (isset($arrOneItem["title"][0]["value"]) ? $arrOneItem["title"][0]["value"] : "");
+        		        $arrMessage["post_description"] = (isset($arrOneItem["description"][0]["value"]) ? $arrOneItem["description"][0]["value"] : "");
+        		        $arrMessage["post_link"] = (isset($arrOneItem["link"][0]["value"]) ? $arrOneItem["link"][0]["value"] : "");
+            
+        	   	        $strContent .= $this->objTemplate->fillTemplate($arrMessage, $strPostTemplateID);
+    
+        	   	        if(++$intCounter >= $this->arrElementData["int1"])
+        	   	           break;
+        		    
+        		    }
+		        }
+		        
+		        //atom feed
+		        if(isset($arrFeed["feed"]) && isset($arrFeed["feed"][0]["entry"])) {
+		              
+		            
+		            $arrTemplate["feed_title"] = $arrFeed["feed"][0]["title"][0]["value"];
+                    $arrTemplate["feed_link"] = $arrFeed["feed"][0]["link"][0]["attributes"]["href"];
+                    $arrTemplate["feed_description"] = $arrFeed["feed"][0]["subtitle"][0]["value"];
+                    $intCounter = 0;
+                    
+                    foreach ($arrFeed["feed"][0]["entry"] as $arrOneItem) {
+                        
+                        $arrMessage = array();
+                        $arrMessage["post_date"] = (isset($arrOneItem["updated"][0]["value"]) ? $arrOneItem["updated"][0]["value"] : "");
+                        $arrMessage["post_title"] = (isset($arrOneItem["title"][0]["value"]) ? $arrOneItem["title"][0]["value"] : "");
+                        $arrMessage["post_description"] = (isset($arrOneItem["summary"][0]["value"]) ? $arrOneItem["summary"][0]["value"] : "");
+                        $arrMessage["post_link"] = (isset($arrOneItem["link"][0]["attributes"]["href"]) ? $arrOneItem["link"][0]["attributes"]["href"] : "");
+            
+                        $strContent .= $this->objTemplate->fillTemplate($arrMessage, $strPostTemplateID);
+    
+                        if(++$intCounter >= $this->arrElementData["int1"])
+                           break;
+                    
+                    }
+		        }
 		    }
 		    else {
 		        $strContent = $this->getText("rssfeed_errorparsing");
