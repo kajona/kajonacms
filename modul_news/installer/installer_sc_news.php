042f5703f345bf862301c587b821e2654f9fdc85
sidler <sidler@mulchprod.de>
2008-01-25 12:40:54 +0000

    BUG: navi-link references index site instead of NULL

--- modul_news/installer/installer_sc_news.php
+++ modul_news/installer/installer_sc_news.php
@@ -161,7 +161,7 @@ class class_installer_sc_news implements interface_sc_installer  {
             
             $objNaviPoint = new class_modul_navigation_point();
             $objNaviPoint->setStrName("News");
-            $objNaviPoint->setStrPageI("");
+            $objNaviPoint->setStrPageI("index");
             $objNaviPoint->saveObjectToDb($strTreeId);
             $strNewsPointID = $objNaviPoint->getSystemid();
             $strReturn .= "ID of new navigation point: ".$objNaviPoint->getSystemid()."\n";
