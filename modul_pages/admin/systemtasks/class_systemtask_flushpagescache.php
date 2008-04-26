6f72621cb8cea4500136ca6820e06efc4cd2fa94
sidler <sidler@mulchprod.de>
2008-04-26 17:27:28 +0000

    NEW: 0000231: systemtask to flush the images cache NEW: flush pagecache moved from the pages-admin-part into its own systemtask

---
+++ modul_pages/admin/systemtasks/class_systemtask_flushpagescache.php
@@ -0,0 +1,71 @@
+<?php
+/*"******************************************************************************************************
+*   (c) 2004-2006 by MulchProductions, www.mulchprod.de                                                 *
+*   (c) 2007-2008 by Kajona, www.kajona.de                                                              *
+*       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
+*-------------------------------------------------------------------------------------------------------*
+*                                                                                                       *
+*   class_systemtask_flushpagescache.php                                                                *
+*   Deletes all cached pages                                                                            *
+*                                                                                                       *
+*-------------------------------------------------------------------------------------------------------*
+*   $Id: class_login_admin.php 1884 2007-12-26 15:04:48Z sidler $                                       *
+********************************************************************************************************/
+
+//base class and interface
+include_once(_adminpath_."/systemtasks/class_systemtask_base.php");
+include_once(_adminpath_."/systemtasks/interface_admin_systemtask.php");
+
+/**
+ * Flushes the cache holding generated pages
+ *
+ * @package modul_system
+ */
+class class_systemtask_flushpagescache extends class_systemtask_base implements interface_admin_systemtask {
+
+
+    /**
+     * contructor to call the base constructor
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    
+    /**
+     * @see interface_admin_systemtast::getStrInternalTaskName()
+     * @return string
+     */
+    public function getStrInternalTaskName() {
+        return "flushpagescache";
+    }
+    
+    /**
+     * @see interface_admin_systemtast::getStrTaskName()
+     * @return string
+     */
+    public function getStrTaskName() {
+        return $this->getText("systemtask_flushpagescache_name");
+    }
+    
+    /**
+     * @see interface_admin_systemtast::executeTask()
+     * @return string
+     */
+    public function executeTask() {
+        include_once(_systempath_."/class_pagecache.php");
+        $objPagecache = new class_pagecache();
+        $objPagecache->flushCompletePagesCache();
+        return $this->getText("systemtask_flushpagescache_done");
+    }
+
+    /**
+     * @see interface_admin_systemtast::getAdminForm()
+     * @return string 
+     */
+    public function getAdminForm() {
+        return "";
+    }
+    
+}
+?>
---
+++ modul_pages/texte/admin/modul_system/texte_systaskflushpagescache_de.php
@@ -0,0 +1,20 @@
+<?php
+/*"******************************************************************************************************
+*   (c) 2004-2006 by MulchProductions, www.mulchprod.de                                                 *
+*   (c) 2007-2008 by Kajona, www.kajona.de                                                              *
+*       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
+*-------------------------------------------------------------------------------------------------------*
+*                                                                                                       *
+*   texte_systemtask_flushpagescache_en.php                                                             *
+*   Admin language file for the flushpagescache systemtask                                              *
+*                                                                                                       *
+*-------------------------------------------------------------------------------------------------------*
+*   $Id: texte_system_de.php 1987 2008-03-21 10:42:27Z sidler $                                         *
+********************************************************************************************************/ 
+
+
+$text["systemtask_flushpagescache_name"]                   = "Seitencache leeren";
+
+$text["systemtask_flushpagescache_done"]                   = "Leeren abgeschlossen.";
+
+?>
