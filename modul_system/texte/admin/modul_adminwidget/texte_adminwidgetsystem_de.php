ec80265c27ca8eea97147b98731a29b7fd34ce67
sidler <sidler@mulchprod.de>
2008-10-21 19:55:00 +0000

    Consolidated systems' widget textfiles

--- modul_system/texte/admin/modul_adminwidget/texte_adminwidgetsystem_de.php
+++ modul_system/texte/admin/modul_adminwidget/texte_adminwidgetsystem_de.php
@@ -6,11 +6,17 @@
 *-------------------------------------------------------------------------------------------------------*
 * 																										*
 * 	texte_adminwidgetsysinfo_de.php	      																*
-* 	Admin language file for sysinfo widget																*
+* 	Admin language file for syslog widget																*
 *																										*
 *-------------------------------------------------------------------------------------------------------*
 *	$Id$								*
 ********************************************************************************************************/
+
+$text["syslog_name"]                         = "Systemlog";
+$text["syslog_nrofrows"]                     = "Anzahl Einträge:";
+
+$text["note_name"]                        = "Notizen";
+$text["note_content"]                     = "Inhalt:";
 
 $text["sysinfo_name"]                        = "Systeminformationen";
 $text["sysinfo_checkboxphp"]                 = "PHP Informationen anzeigen";
@@ -25,6 +31,20 @@ $text["sysinfo_server_diskspacef"]           = "Freier Plattenplatz: ";
 $text["sysinfo_kajona_version"]              = "Kajona Version: ";
 $text["sysinfo_kajona_versionAvail"]         = "Neuste Version: ";
 $text["sysinfo_kajona_nrOfModules"]          = "Anzahl Kajonamodule: ";
+
+$text["sysinfo_name"]                        = "Systeminformation";
+$text["sysinfo_checkboxphp"]                 = "Show PHP information";
+$text["sysinfo_checkboxserver"]              = "Show server information";
+$text["sysinfo_checkboxkajona"]              = "Show kajona information";
+
+$text["sysinfo_php_version"]                 = "PHP Version: ";
+$text["sysinfo_php_memlimit"]                = "PHP Memory Limit: ";
+$text["sysinfo_server_system"]               = "System: ";
+$text["sysinfo_server_diskspace"]            = "Total Diskpace: ";
+$text["sysinfo_server_diskspacef"]           = "Free Diskspace: ";
+$text["sysinfo_kajona_version"]              = "Kajona Version: ";
+$text["sysinfo_kajona_versionAvail"]         = "Latest Version: ";
+$text["sysinfo_kajona_nrOfModules"]          = "Number of modules: ";
 
 
 ?>
--- modul_system/texte/admin/modul_adminwidget/texte_adminwidgetsystem_en.php
+++ modul_system/texte/admin/modul_adminwidget/texte_adminwidgetsystem_en.php
@@ -5,12 +5,33 @@
 *       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
 *-------------------------------------------------------------------------------------------------------*
 * 																										*
-* 	texte_adminwidgetsystemcheck_en.php	      															*
-* 	Admin language file for systemcheck widget															*
+* 	texte_adminwidgetsysinfo_en.php	      																*
+* 	Admin language file for syslog widget																*
 *																										*
 *-------------------------------------------------------------------------------------------------------*
-*	$Id$							*
+*	$Id$								*
 ********************************************************************************************************/
+
+
+$text["syslog_name"]                         = "Systemlog";
+$text["syslog_nrofrows"]                     = "Number of entries:";
+
+$text["note_name"]                        = "Notes";
+$text["note_content"]                     = "Content:";
+
+$text["sysinfo_name"]                        = "Systeminformation";
+$text["sysinfo_checkboxphp"]                 = "Show PHP information";
+$text["sysinfo_checkboxserver"]              = "Show server information";
+$text["sysinfo_checkboxkajona"]              = "Show kajona information";
+
+$text["sysinfo_php_version"]                 = "PHP Version: ";
+$text["sysinfo_php_memlimit"]                = "PHP Memory Limit: ";
+$text["sysinfo_server_system"]               = "System: ";
+$text["sysinfo_server_diskspace"]            = "Total Diskpace: ";
+$text["sysinfo_server_diskspacef"]           = "Free Diskspace: ";
+$text["sysinfo_kajona_version"]              = "Kajona Version: ";
+$text["sysinfo_kajona_versionAvail"]         = "Latest Version: ";
+$text["sysinfo_kajona_nrOfModules"]          = "Number of modules: ";
 
 $text["systemcheck_name"]                        = "Systemcheck";
 $text["systemcheck_checkboxphp"]                 = "PHP Check";
