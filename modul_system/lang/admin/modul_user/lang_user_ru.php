cdc936a7ea0510c96b2c5644381b901e6c4eec86
jschroeter <jakob@schroetermedien.de>
2009-03-01 12:01:28 +0000

    NEW: enabled russian admin language  - added first russian translation: module user (thanks to Ksenia and Michael!)

---
+++ modul_system/lang/admin/modul_user/lang_user_ru.php
@@ -0,0 +1,129 @@
+<?php
+/*"******************************************************************************************************
+*   (c) 2004-2006 by MulchProductions, www.mulchprod.de                                                 *
+*   (c) 2007-2009 by Kajona, www.kajona.de                                                              *
+*       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
+*-------------------------------------------------------------------------------------------------------*
+*	$Id$					    *
+********************************************************************************************************/
+//Edited with Kajona Language Editor GUI, see www.kajona.de and www.mulchprod.de for more information
+//Kajona Language Editor Core Build 147
+
+//non-editable entries
+$lang["permissions_header"]              = array(0 => "Просмотр", 1 => "Изменить", 2 => "Удалить", 3 => "Permissions", 4 => "Logs", 5 => "", 6 => "", 7 => "", 8 => "");
+
+//editable entries
+$lang["_admin_gruppe_id_"]               = "ID группы админа:";
+$lang["_admin_skin_default_"]            = "Скин админа по умолчанию:";
+$lang["_guests_group_id_"]               = "ID гостевой группы:";
+$lang["_user_log_nrofrecords_"]          = "Количество строк:";
+$lang["_user_log_nrofrecords_hint"]      = "Определяет количество строк, которые будут показаны в логин протоколе.";
+$lang["_user_selfedit_"]                 = "Личные данные:";
+$lang["_user_selfedit_hint"]             = "Устанавливает, может ли пользователь изменять свои собственные данные.";
+$lang["admin"]                           = "Админ логин:";
+$lang["aktiv"]                           = "Активный:";
+$lang["email"]                           = "Email:";
+$lang["fehler_recht"]                    = "Недостаточно прав для совершения действия";
+$lang["fehler_speichern"]                = "Ошибка при сохранении профайла!";
+$lang["gebdatum"]                        = "Дата рождения:";
+$lang["group_memberlist"]                = "Участники группы ";
+$lang["gruppe"]                          = "Название группы:";
+$lang["gruppe_anlegen_erfolg"]           = "Группа была успешно сохранена";
+$lang["gruppe_anlegen_fehler"]           = "Ошибка при сохранении группы";
+$lang["gruppe_anlegen_fehler_name"]      = "Пожалуйста, введите название группы";
+$lang["gruppe_bearbeiten"]               = "Изменить группу";
+$lang["gruppe_bearbeiten_x"]             = "Эта группа не может быть изменена";
+$lang["gruppe_loeschen_erfolg"]          = "Группа успешно удалена";
+$lang["gruppe_loeschen_fehler"]          = "Ошибка при удалении группы";
+$lang["gruppe_loeschen_frage"]           = "Действительно удалить группу?";
+$lang["gruppe_loeschen_x"]               = "Группа не может быть удалена";
+$lang["gruppe_mitglieder"]               = "Показать участников группы";
+$lang["gruppen"]                         = "Группы: ";
+$lang["gruppen_anlegen"]                 = "Создать группу";
+$lang["gruppen_liste"]                   = "Все группы";
+$lang["handy"]                           = "Мобильный телефон:";
+$lang["lang_de"]                         = "Deutsch";
+$lang["lang_en"]                         = "English";
+$lang["lang_fr"]                         = "Французский";
+$lang["lang_pt"]                         = "Португальский";
+$lang["lang_ru"]                         = "Русский";
+$lang["language"]                        = "Язык админа:";
+$lang["log"]                             = "Логи:";
+$lang["login_dashboard"]                 = "Главная страница";
+$lang["login_datum"]                     = "Дата";
+$lang["login_ip"]                        = "IP-Адрес";
+$lang["login_loginButton"]               = "Логин";
+$lang["login_loginCookiesInfo"]          = "Пожалуйста, разрешите cookies для этого сайта и обновите текущую станицу чтобы стало возможным использование всех функций.";
+$lang["login_loginError"]                = "К сожалению, введенные данные недействительны. Пожалуйста, проверьте правильность написания имя пользователя и пароля. Если вы все равно не можете ввойти в систему, сообщите своему системному администратору.";
+$lang["login_loginJsInfo"]               = "Пожалуйста, разрешите использование JavaScript для этого сайта и обновите текущую страницу чтобы стало возможным использование всех функций.";
+$lang["login_loginPass"]                 = "Пароль";
+$lang["login_loginTitle"]                = "Логин";
+$lang["login_loginUser"]                 = "Имя пользователя";
+$lang["login_logoutTitle"]               = "Выйти";
+$lang["login_nr"]                        = "№ ";
+$lang["login_profileTitle"]              = "Изменить личные данные";
+$lang["login_status"]                    = "Статус";
+$lang["login_statusTitle"]               = "Авторизирован как:";
+$lang["login_status_0"]                  = "Ошибка при авторизации";
+$lang["login_status_1"]                  = "Авторизация ОК";
+$lang["login_user"]                      = "Пользователь";
+$lang["loginlog"]                        = "Логин лог";
+$lang["mitglied_loeschen"]               = "Удалить участника из группы";
+$lang["mitglied_loeschen_erfolg"]        = "Участник был успешно удален из группы";
+$lang["mitglied_loeschen_fehler"]        = "Ошибка при удалении участника из группы";
+$lang["mitglied_loeschen_frage_1"]       = "Действительно удалить участника из группы?";
+$lang["mitglied_loeschen_frage_2"]       = "Удалить?";
+$lang["mitglied_speichern_erfolg"]       = "Участие успешно сохранено";
+$lang["mitglied_speichern_fehler"]       = "Ошибка при сохранении участия";
+$lang["modul_rechte"]                    = "Разрешения";
+$lang["modul_titel"]                     = "Пользователь";
+$lang["nachname"]                        = "Фамилия:";
+$lang["ort"]                             = "Город:";
+$lang["passwort"]                        = "Пароль:";
+$lang["passwort2"]                       = "Пароль:";
+$lang["plz"]                             = "Индекс:";
+$lang["portal"]                          = "Логин портала:";
+$lang["quickhelp_edit"]                  = "Эта форма используется чтобы изменить существующего или создать нового пользователя. Если пользователь имеет статус активного, то ему разрешено авторизироваться в системе. Дополнительно, поле для логина должно быть заполнено.";
+$lang["quickhelp_grouplist"]             = "Сущесвующие группы перечислены здесь. Группы гостей и админов не могут быть удалены, так как они необходимы для правильного функционирования системы.";
+$lang["quickhelp_groupmember"]           = "Все участники группы перечислены здесь. Чтобы закончить чье-либо участие, принадлежность пользователя к группе может быть удалено. Если вы хотите, чтобы какой-либо пользователь стал участником группы, используйте диалоговое окно группы.";
+$lang["quickhelp_groupnew"]              = "Чтобы создать группу, вам только необходимо указать ее имя.";
+$lang["quickhelp_groupsave"]             = "Чтобы создать группу, вам только необходимо указать ее имя.";
+$lang["quickhelp_list"]                  = "Все созданные пользователи перечислены здесь. Вы можете изменить личные пользователя и его участие в группах.";
+$lang["quickhelp_loginlog"]              = "Логин лог показывает список всех успешных и неудавшихся попыток авторизироваться. Используя этот лог файл, вы можите увидеть кто авторизировался в системе.";
+$lang["quickhelp_membership"]            = "Используя этот список, принадлежность к какой-либо группе может быть изменено, а также пользователь может быть добавлен в или удален из группы. Пользователь, которые не состоит ни в одной группе, не имеет право на авторизацию.";
+$lang["quickhelp_new"]                   = "Эта форма используется чтобы изменить существующего или создать нового пользователя. Если пользователь имеет статус активного, то ему разрешается авторизироваться в системе. Дополнительно, поле для логина должно быть заполнено.";
+$lang["required_email"]                  = "Email";
+$lang["required_gruppename"]             = "Имя группы";
+$lang["required_password_equal"]         = "Пароли не совпадают";
+$lang["required_passwort"]               = "Пароль";
+$lang["required_passwort2"]              = "Подтверждение пароля";
+$lang["required_user_existing"]          = "Пользователь с таким именем уже существует";
+$lang["required_username"]               = "Имя пользователя";
+$lang["skin"]                            = "Админ скин:";
+$lang["strasse"]                         = "Улица:";
+$lang["submit"]                          = "Сохранить";
+$lang["tel"]                             = "Телефон:";
+$lang["user"]                            = "Пользователь: ";
+$lang["user_active"]                     = "Изменить статус (активный)";
+$lang["user_anlegen"]                    = "Создать пользователя";
+$lang["user_bearbeiten"]                 = "Настройки пользователя";
+$lang["user_erfolg"]                     = "Пользователь успешно сохранен";
+$lang["user_fehler"]                     = "Ошибка при сохранении пользователя";
+$lang["user_fehler_mail"]                = "Пожалуйста, введите действующий электронный адрес <a href=\"javascript:history.back(-1)\">back</a>";
+$lang["user_fehler_name"]                = "Пожалуйста, введите имя пользователя <a href=\"javascript:history.back(-1)\">back</a>";
+$lang["user_fehler_namedoppelt"]         = "Пользователь с таким именем уже существует, пожалуйста, введите другое <a href=\"javascript:history.back(-1)\">back</a>";
+$lang["user_fehler_pass"]                = "Введенные пароли не совпадают <a href=\"javascript:history.back(-1)\">back</a>";
+$lang["user_inactive"]                   = "Изменить статус (неактивный)";
+$lang["user_lastlogin"]                  = "Последний логин:";
+$lang["user_liste"]                      = "Все пользователи";
+$lang["user_loeschen_erfolg"]            = "Пользователь успешно удален";
+$lang["user_loeschen_fehler"]            = "Ошибка при удалении пользователя";
+$lang["user_loeschen_frage"]             = "Действительно удалить пользователя?";
+$lang["user_logins"]                     = "Логины:";
+$lang["user_memberships"]                = "Участие пользователя в группах";
+$lang["user_personaldata"]               = "Личные данные";
+$lang["user_system"]                     = "Настройки системы";
+$lang["user_zugehoerigkeit"]             = "Участие в группах";
+$lang["username"]                        = "Имя пользователя:";
+$lang["vorname"]                         = "Имя:";
+?>
