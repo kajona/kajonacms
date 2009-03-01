2cc83c2c2ceae63fe40737bda79beeb99fa7a435
jschroeter <jakob@schroetermedien.de>
2009-03-01 14:49:40 +0000

    updated svn properties

--- modul_postacomment/portal/scripts/postacomment.js
+++ modul_postacomment/portal/scripts/postacomment.js
@@ -1,7 +1,7 @@
 //   (c) 2004-2006 by MulchProductions, www.mulchprod.de
 //   (c) 2007-2009 by Kajona, www.kajona.de
 //       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt
-//       $Id: rating.js 2353 2008-12-31 15:22:01Z sidler $ 
+//       $Id$ 
 
 function loadCaptcha() {
 	if (document.getElementById("kajonaCaptcha") == undefined) {
