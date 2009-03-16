9eaff689b7607513bee3f15b5300d02c245e1691
jschroeter <jakob@schroetermedien.de>
2009-03-16 17:55:56 +0000

    NEW: russian translation for module dashboard, filemanager and languages (thanks to Ksenia and Michael!)

---
+++ modul_system/lang/admin/modul_dashboard/lang_dashboard_ru.php
@@ -0,0 +1,30 @@
+<?php
+/*"******************************************************************************************************
+*   (c) 2004-2006 by MulchProductions, www.mulchprod.de                                                 *
+*   (c) 2007-2009 by Kajona, www.kajona.de                                                              *
+*       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
+*-------------------------------------------------------------------------------------------------------*
+*	$Id$					    *
+********************************************************************************************************/
+//Edited with Kajona Language Editor GUI, see www.kajona.de and www.mulchprod.de for more information
+//Kajona Language Editor Core Build 163
+
+//editable entries
+$lang["addWidget"]                       = "Добавить виджет";
+$lang["addWidgetNextStep"]               = "Вперед";
+$lang["addWidgetSave"]                   = "Сохранить";
+$lang["column"]                          = "Колонка:";
+$lang["column1"]                         = "Колонка 1";
+$lang["column2"]                         = "Колонка 2";
+$lang["column3"]                         = "Колонка 3";
+$lang["editWidget"]                      = "Изменить виджет";
+$lang["errorSavingWidget"]               = "Ошибка при сохранении виджета";
+$lang["fehler_recht"]                    = "Недостаточно прав";
+$lang["modul_titel"]                     = "Обзор";
+$lang["moduleRights"]                    = "Разрешения";
+$lang["quickhelp_addWidgetToDashboard"]  = "Чтобы добавить виджет в панель инструментов, вы должны выполнить процесс, состоящий всего из двух шагов. Сначала выберите необходимый виджет и колонку, куда он будет добавлен. Вторым шагом виджет может быть настроен.";
+$lang["quickhelp_list"]                  = "В панель инструментов можно добавить любой вид виджетов. Виджет - это небольшая программа, которая отображает часть информации. Виджеты могут быть упорядочены, изменены или удалены прямо на панеле инструментов. Чтобы добавить новые виджеты, используйте функцию \"Добавить виджет\". ";
+$lang["setDashboardPosition"]            = "Новая позиция успешно сохранена";
+$lang["widget"]                          = "Виджет:";
+$lang["widgetDeleteQuestion"]            = "Вы действительно хотите удалить виджет &quot;<b>%%element_name%%</b>&quot;?";
+?>
