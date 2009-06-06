b6cce113a4b6f8f42ed4e02b954a36f9b77a188f
thertwig <thertwig@kajona.de>
2009-06-06 14:46:39 +0000

    - added index.php to check for PHP version number - no PHP5 specific features like "class" used, so fully functional with PHP4

---
+++ modul_system/installer/index.php
@@ -0,0 +1,15 @@
+<?php
+/*"******************************************************************************************************
+*   (c) 2004-2006 by MulchProductions, www.mulchprod.de                                                 *
+*   (c) 2007-2009 by Kajona, www.kajona.de                                                              *
+*       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
+*-------------------------------------------------------------------------------------------------------*
+*   $Id: index.php 2655 2009-03-28 19:00:05Z sidler $                                                *
+********************************************************************************************************/
+
+// check if PHP version is less than the required version 5
+	if((int)substr(phpversion(),0,1)<5)
+		die("Wrong PHP version");
+	else
+		header("Location: http://". $_SERVER["SERVER_NAME"] .str_replace("index.php", "installer.php", $_SERVER["PHP_SELF"]));
+?>
