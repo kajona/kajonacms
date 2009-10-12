3fa2b436cf4000b900e5aeaa29adbdfdf6dd300d
sidler <sidler@mulchprod.de>
2009-10-12 20:50:25 +0000

    BUG: call to apache_setenv() could fail on non iis and old apache installations. Please check the existance of server-specific function before calling them!!!

--- _debugging/debug/autotest.php
+++ _debugging/debug/autotest.php
@@ -28,7 +28,8 @@ $objCarrier = class_carrier::getInstance();
 echo "|loaded.                                                                        |\n";
 echo "+-------------------------------------------------------------------------------+\n\n";
 
-@apache_setenv('no-gzip', 1);
+if(function_exists("apache_setenv"))
+    @apache_setenv('no-gzip', 1);
 @ini_set('zlib.output_compression', 0);
 @ini_set('implicit_flush', 1);
 for ($i = 0; $i < ob_get_level(); $i++) { ob_end_flush(); }
