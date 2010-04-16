b2298c00dfc9f3fce02a6e1a1e2dad88c25ed7df
jschroeter <jakob@schroetermedien.de>
2010-04-16 21:54:27 +0000

    NEW: installer checks write-permissions for /portal/downloads

--- modul_system/installer/installer.php
+++ modul_system/installer/installer.php
@@ -127,7 +127,8 @@ class class_installer {
 	                                "/system/dbdumps",
 	                                "/system/debug",
 	                                "/portal/pics/cache",
-	                                "/portal/pics/upload");
+	                                "/portal/pics/upload",
+	                                "/portal/downloads");
 
 	    $arrModules = array("mbstring",
 	                        "gd",
