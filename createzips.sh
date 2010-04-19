e6627b240acf768604e4de9da9df2a96ba865511
sidler <sidler@mulchprod.de>
2010-04-19 19:25:05 +0000

    BUG: script includes dot-files

--- createzips.sh
+++ createzips.sh
@@ -2,52 +2,107 @@
 
 
 
-echo "deleting zips\n"
-rm *.zip;
-
-echo "single zips...\n";
-for i in `ls "$PWD"`
-do
-  if [ -d "$PWD/$i" ]
+init() 
+{ 
+  echo "deleting target folder temp...\n";
+  if [ -d "$PWD/packages" ]
   then
-      cd "$i";
-      zip -r "../$i" *
-      cd "..";
+    rm -rf "$PWD/packages"
   fi
-done
+  mkdir "$PWD/packages"
 
-echo "full package...\n";
-mkdir "kajona";
-for i in `ls "$PWD"`
-do
-  if [ -d "$PWD/$i" ] && [ "$i" != "kajona" ] && [ "$i" != "_debugging" ]
-  then
-    cd $i;
-    cp -Rf * "../kajona/";
-    cd "..";
-  fi
-done
+  echo "creating exported folders...\n";
+  for i in `ls -a $PWD`
+  do
+    if [ -d $i ] && [ $i != ".svn" ] && [ $i != "packages" ] && [ $i != "kajona" ] && [ $i != "." ] && [ $i != ".." ]
+    then
+      echo $i
+      mkdir "$PWD/packages/$i"
+      copyRecursive "$PWD/$i" "$PWD/packages/$i"
+    fi
+  done
+  
+}
 
-cd "kajona";
-zip -r "../kajona-full.zip" *;
-cd "..";
-rm -rf "kajona";
 
-echo "light package...\n";
-mkdir "kajona";
-for i in modul_navigation modul_pages modul_samplecontent modul_system
-do
-  if [ -d "$PWD/$i" ] && [ "$i" != "kajona" ] && [ "$i" != "_debugging" ]
-  then
-    echo $i;
-    cd $i;
-    cp -Rf * "../kajona/";
-    cd "..";
-  fi
-done
+singleZips() 
+{
+  cd "packages"
+  echo "single zips @ $PWD...\n";
+  for i in `ls "$PWD"`
+  do
+    if [ -d "$PWD/$i" ] 
+    then
+	zip -r $i $i
+    fi
+  done
+  cd ".."
+}
 
-cd "kajona";
-zip -r "../kajona-lite.zip" *;
-cd "..";
-rm -rf "kajona";
+fullPackage()
+{
+  cd "packages"
+  echo "full package...\n";
+  mkdir "kajona";
+  for i in `ls -a "$PWD"`
+  do
+    if [ -d "$PWD/$i" ] && [ $i != "kajona" ] && [ $i != "_debugging" ] && [ $i != "." ] && [ $i != ".." ]
+    then
+      echo $i
+      copyRecursive "$PWD/$i" "$PWD/kajona/"
+    fi
+  done
+  
+  zip -r kajona-full.zip kajona;
+  rm -rf "kajona";
+  cd ".."
+}
+
+
+litePackage()
+{ 
+  cd "packages"
+  echo "light package...\n";
+  mkdir "kajona";
+  for i in modul_navigation modul_pages modul_samplecontent modul_system
+  do
+    if [ -d "$PWD/$i" ] && [ $i != "kajona" ] && [ $i != "_debugging" ]
+    then
+      echo $i
+      copyRecursive "$PWD/$i" "$PWD/kajona/"
+    fi
+  done
+
+  zip -r kajona-lite.zip kajona
+  rm -rf "kajona";
+  cd ".."
+}
+
+copyRecursive()
+{ #startDir = 1, targetDir = 2
+  for i in `ls -a $1`
+  do
+    if [ -d "$1/$i" ] && [ $i != ".svn" ] && [ $i != "." ] && [ $i != ".." ]
+    then
+      if [ ! -d "$2/$i" ]
+      then
+	mkdir "$2/$i"
+      fi
+      copyRecursive "$1/$i" "$2/$i"
+    fi
+
+    if [ -f "$1/$i" ]
+    then
+      cp "$1/$i" "$2/$i"
+    fi
+
+  done
+}
+
+echo "Packaging...\n";
+init
+singleZips
+litePackage
+fullPackage
+
 
