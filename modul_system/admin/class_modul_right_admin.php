8e28956970b7e90abbfb0fe991da71a4a3411147
sidler <sidler@mulchprod.de>
2010-07-27 20:41:02 +0000

    BUG: when checking right, the existance of the constant "_pages_folder_id_" is checked before being used. in some cases, there are installations without the pages-module.

--- modul_system/admin/class_modul_right_admin.php
+++ modul_system/admin/class_modul_right_admin.php
@@ -113,7 +113,7 @@ class class_modul_right_admin extends class_admin implements interface_admin {
 			//Load the rights header-row
 			if($arrSystemRecord["system_module_nr"] == 0)
 			    $strModule = "system";
-            else if($arrSystemRecord["system_module_nr"] == _pages_folder_id_)
+            else if(defined("_pages_folder_id_") && $arrSystemRecord["system_module_nr"] == _pages_folder_id_)
                 $strModule = "pages";
 			else {
 			    $strTempId = class_modul_system_module::getModuleIdByNr($arrSystemRecord["system_module_nr"]);
