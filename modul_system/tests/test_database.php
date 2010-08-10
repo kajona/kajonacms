6b1c3dcfc3714d9ac948381dda4492263bfdf500
sidler <sidler@mulchprod.de>
2010-08-10 18:56:55 +0000

    enhanced database layer test

--- modul_system/tests/test_database.php
+++ modul_system/tests/test_database.php
@@ -31,11 +31,35 @@ class class_test_database implements interface_testable {
             $strQuery = "INSERT INTO "._dbprefix_."temp_autotest
                 (temp_id, temp_long, temp_double, temp_char10, temp_char20, temp_char100, temp_char254, temp_char500, temp_text)
                 VALUES
-                ('".generateSystemid()."', 123456".$intI.", 23.45".$intI.", 'char10".$intI."', 'char20".$intI."', 'char100".$intI."', 'char254".$intI."', 'char500".$intI."', 'text".$intI."')";
+                ('".generateSystemid()."', 123456".$intI.", 23.45".$intI.", '".$intI."', 'char20".$intI."', 'char100".$intI."', 'char254".$intI."', 'char500".$intI."', 'text".$intI."')";
 
             class_assertions::assertTrue($objDB->_query($strQuery), "testDataBase insert");
         }
 
+
+        echo "\tgetRow test\n";
+        $strQuery = "SELECT * FROM "._dbprefix_."temp_autotest ORDER BY temp_long ASC";
+        $arrRow = $objDB->getRow($strQuery);
+        class_assertions::assertEqual(count($arrRow) , 18, "testDataBase getRow count");
+        class_assertions::assertEqual($arrRow["temp_char10"] , "1", "testDataBase getRow content");
+        
+        echo "\tgetArray test\n";
+        $strQuery = "SELECT * FROM "._dbprefix_."temp_autotest ORDER BY temp_long ASC";
+        $arrRow = $objDB->getArray($strQuery);
+        class_assertions::assertEqual(count($arrRow) , 50, "testDataBase getArray count");
+
+        $intI = 1;
+        foreach($arrRow as $arrSingleRow)
+            class_assertions::assertEqual($arrSingleRow["temp_char10"] , $intI++, "testDataBase getArray content");
+        
+        echo "\tgetArraySection test\n";
+        $strQuery = "SELECT * FROM "._dbprefix_."temp_autotest ORDER BY temp_long ASC";
+        $arrRow = $objDB->getArraySection($strQuery, 0, 9);
+        class_assertions::assertEqual(count($arrRow) , 10, "testDataBase getArraySection count");
+
+        $intI = 1;
+        foreach($arrRow as $arrSingleRow)
+            class_assertions::assertEqual($arrSingleRow["temp_char10"] , $intI++, "testDataBase getArraySection content");
 
 
         echo "\tdeleting table\n";
