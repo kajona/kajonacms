ae42aaf9e843ba9d1d4c92c0ece375cceb442381
sidler <sidler@mulchprod.de>
2010-09-20 19:47:07 +0000

    Fixed phpdoc

--- modul_system/admin/class_toolkit_admin.php
+++ modul_system/admin/class_toolkit_admin.php
@@ -272,6 +272,7 @@ class class_toolkit_admin extends class_toolkit {
      * @param string $strName
      * @param string $strTitle
      * @param string $strValue
+     * @param string $strClass
      * @param string $strOpener
      * @param bool $bitReadonly
      * @return string
