c4c91627993804d79ce010c958c884b6b4c6291c
sidler <sidler@mulchprod.de>
2011-02-17 14:40:33 +0000

    updated phpunit test to new changelog structure

--- modul_system/tests/test_systemchangelogTest.php
+++ modul_system/tests/test_systemchangelogTest.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once (dirname(__FILE__)."/../system/class_testbase.php");
+require_once (dirname(__FILE__)."/../system/interface_versionable.php");
 
 /**
  * Tests a few aspects of the changelog-component
@@ -18,21 +19,67 @@ class test_systemchangelogTest extends class_testbase {
 
         $objChanges = new class_modul_system_changelog();
 
-        $objChanges->createLogEntry("autotest", "test", $strSystemid, "autotest", "0", "1");
+        $objChanges->createLogEntry(new dummyObject($strSystemid), "1");
         $this->assertEquals(1, class_modul_system_changelog::getLogEntriesCount($strSystemid));
 
         class_carrier::getInstance()->getObjDB()->flushQueryCache();
 
-        $objChanges->createLogEntry("autotest", "test", $strSystemid, "autotest", "3", "3");
+        $objChanges->createLogEntry(new dummyObject($strSystemid), "2");
         $this->assertEquals(1, class_modul_system_changelog::getLogEntriesCount($strSystemid));
 
         class_carrier::getInstance()->getObjDB()->flushQueryCache();
 
-        $objChanges->createLogEntry("autotest", "test", $strSystemid, "autotest", "3", "3", true);
+        $objChanges->createLogEntry(new dummyObject($strSystemid), "2", true);
         $this->assertEquals(2, class_modul_system_changelog::getLogEntriesCount($strSystemid));
 
         $objSystemCommon->deleteSystemRecord($strSystemid);
 
     }
+}
+
+
+class dummyObject implements interface_versionable {
+    private $strSystemid;
+    function __construct($strSystemid) {
+        $this->strSystemid = $strSystemid;
+    }
+
+    public function getSystemid() {
+        return $this->strSystemid;
+    }
+
+    public function setStrSystemid($strSystemid) {
+        $this->strSystemid = $strSystemid;
+    }
+
+
+    public function getActionName($strAction) {
+        return "dummy";
+    }
+
+    public function getChangedFields($strAction) {
+        if($strAction == "1")
+            return array(array("property" => "test", "oldvalue" => "0", "newvalue" => "1"));
+
+        if($strAction == "2")
+            return array(array("property" => "test", "oldvalue" => "3", "newvalue" => "3"));
+    }
+
+    public function getClassname() {
+        return __CLASS__;
+    }
+
+    public function getModuleName() {
+        return "dummy";
+    }
+
+    public function getPropertyName($strProperty) {
+        return $strProperty;
+    }
+
+    public function getRecordName() {
+        return "dummy";
+    }
+
 }
 ?>
