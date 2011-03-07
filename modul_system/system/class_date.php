97facb0b67726fe3b43313c1827d889c7fa2cb7a
sidler <sidler@mulchprod.de>
2011-03-07 15:06:19 +0000

    NEW: class_date::getIntDayOfWeek() returns the day of the week, starting with 0 as sunday and ending with 6 as saturday. see phps' date('w', ...) function for details

--- modul_system/system/class_date.php
+++ modul_system/system/class_date.php
@@ -136,6 +136,24 @@ class class_date {
     }
 
     /**
+     * Returns the integer-based number of the day of the week.
+     * 0 is sunday whereas 6 is the saturday.
+     * This leads to:
+     *   0 => Sunday
+     *   1 => Monday
+     *   2 => Tuesday
+     *   3 => Wednesday
+     *   4 => Thursday
+     *   5 => Friday
+     *   6 => Saturday
+     *
+     * @return int
+     */
+    public function getIntDayOfWeek() {
+        return date('w', $this->getTimeInOldStyle());
+    }
+
+    /**
      * Swap the year part
      *
      * @param int $intYear
