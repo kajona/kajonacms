c3ac1b14024a49f690853aa57841f0b8a6f0aee5
sidler <sidler@mulchprod.de>
2011-10-29 12:02:19 +0000

    NEW: Updated minimal system-version for modules to 3.4.1 since the usersubssytem changed.

--- element_tellafriend/installer/installer_element_tellafriend.php
+++ element_tellafriend/installer/installer_element_tellafriend.php
@@ -32,7 +32,7 @@ class class_installer_element_tellafriend extends class_installer_base implement
 	}
 
     public function getMinSystemVersion() {
-	    return "3.4.0";
+	    return "3.4.1";
 	}
 
 	public function hasPostInstalls() {
