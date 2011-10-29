2b27d175f1b927ba4aefcf8c85662bb63ce48dba
sidler <sidler@mulchprod.de>
2011-10-29 12:02:51 +0000

    NEW: Updated minimal system-version for modules to 3.4.1 since the usersubssytem changed.

--- modul_rating/installer/installer_rating.php
+++ modul_rating/installer/installer_rating.php
@@ -30,7 +30,7 @@ class class_installer_rating extends class_installer_base implements interface_i
 	}
 
     public function getMinSystemVersion() {
-	    return "3.3.1.8";
+	    return "3.4.1";
 	}
 
 	public function hasPostInstalls() {
