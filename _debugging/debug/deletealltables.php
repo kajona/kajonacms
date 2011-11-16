06f8a7f6b006c54027fbfa9f567832b952a75e26
sidler <sidler@mulchprod.de>
2011-11-16 10:22:23 +0000

    NEW: added a debug-script to delete all tables of the current installation. requires the credentials  of a member of the admin-group.

---
+++ _debugging/debug/deletealltables.php
@@ -0,0 +1,86 @@
+<?php
+/*"******************************************************************************************************
+*   (c) 2004-2006 by MulchProductions, www.mulchprod.de                                                 *
+*   (c) 2007-2011 by Kajona, www.kajona.de                                                              *
+*       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
+*-------------------------------------------------------------------------------------------------------*
+*	$Id: dbquery.php 3530 2011-01-06 12:30:26Z sidler $                                     *
+********************************************************************************************************/
+
+header("Content-Type: text/html; charset=utf-8");
+require_once("../system/includes.php");
+
+
+echo "<pre>\n";
+echo "+-------------------------------------------------------------------------------+\n";
+echo "| Kajona Debug Subsystem                                                        |\n";
+echo "|                                                                               |\n";
+echo "| Delete all tables                                                             |\n";
+echo "|                                                                               |\n";
+echo "+-------------------------------------------------------------------------------+\n";
+echo "|loading system kernel...                                                       |\n";
+
+$objCarrier = class_carrier::getInstance();
+
+echo "|loaded.                                                                        |\n";
+echo "+-------------------------------------------------------------------------------+\n\n";
+
+if(issetPost("dodelete")) {
+	$strUsername = getPost("username");
+	$strPassword = getPost("password");
+
+    $objUsersource = new class_modul_user_sourcefactory();
+    $objUser = $objUsersource->getUserByUsername($strUsername);
+    echo "Authenticating user...\n";
+    if($objUsersource->authenticateUser($strUsername, $strPassword)) {
+        echo " ... authenticated.\n";
+        $arrGroupIds = $objUser->getArrGroupIds();
+        if(in_array(_admins_group_id_, $arrGroupIds)) {
+            echo "User is member of admin-group.\n";
+
+            $arrTables = class_carrier::getInstance()->getObjDB()->getTables();
+            foreach($arrTables as $strOneTable) {
+                $strQuery = "DROP TABLE ".$strOneTable;
+                echo " executing ".$strQuery."\n";
+                class_carrier::getInstance()->getObjDB()->_pQuery($strQuery, array());
+            }
+
+        }
+        else {
+            echo "User is not a member of the admin-group!\n";
+        }
+    }
+    else
+        echo "Authentication failed!\n";
+
+
+}
+else {
+
+    $arrTables = class_carrier::getInstance()->getObjDB()->getTables();
+
+	echo "ATTENTION: This script will delete all tables of you current installation.\n\n";
+	echo "To perform this action, you have to provide the credentials of a member of the admin-group.\n\n";
+
+    echo "<form method=\"post\">";
+	echo "Username: <input type='text' name='username'><br />";
+	echo "Password: <input type='password' name='password'>";
+	echo "<input type=\"hidden\" name=\"dodelete\" value=\"1\" /><br /><br />";
+	echo "<input type=\"submit\" value=\"Delete tables\" />";
+	echo "</form>";
+
+	echo "Currently, this will include the following tables:";
+    echo "\n -".implode("\n -", $arrTables);
+
+
+}
+
+
+echo "\n\n";
+echo "+-------------------------------------------------------------------------------+\n";
+echo "| (c) www.kajona.de                                                             |\n";
+echo "+-------------------------------------------------------------------------------+\n";
+echo "</pre>";
+
+
+?>
