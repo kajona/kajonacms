38294b4908e59202d6b11e41d6ffbab4510db914
sidler <sidler@mulchprod.de>
2011-11-20 18:59:28 +0000

    removed old scripts

--- local_backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# sidler, 2006
-#
-# Kopiert alle Dateien in ein lokales Zip-archiv	
- 
-
-tar -czf ./backup_$(date +%Y%m%d_%H%M).tar.gz ./kajona
-echo "created file backup_$(date +%Y%m%d_%H%M).tar.gz"
