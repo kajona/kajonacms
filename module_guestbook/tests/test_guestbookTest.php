15e8bf857de3b51ae102dbe5fcece99aca196ef0
sidler <sidler@mulchprod.de>
2012-04-13 12:54:34 +0000

    Fixed unit-test

--- module_guestbook/tests/test_guestbookTest.php
+++ module_guestbook/tests/test_guestbookTest.php
@@ -10,45 +10,45 @@ class class_test_guestbook extends class_testbase  {
         echo "\tmodul_guestbook...\n";
 
         echo "creating a new guestbook, moderated...\n";
-        $objGuestbook = new class_modul_guestbook_guestbook();
-        $objGuestbook->setGuestbookTitle("test guestbook");
-        $objGuestbook->setGuestbookModerated(0);
+        $objGuestbook = new class_module_guestbook_guestbook();
+        $objGuestbook->setStrGuestbookTitle("test guestbook");
+        $objGuestbook->setIntGuestbookModerated(1);
 
         $objGuestbook->updateObjectToDb();
         $strGBId = $objGuestbook->getSystemid();
 
         echo "adding a new post...\n";
-        $objPost = new class_modul_guestbook_post();
-        $objPost->setGuestbookPostName("subject");
-        $objPost->setGuestbookPostText("test");
+        $objPost = new class_module_guestbook_post();
+        $objPost->setStrGuestbookPostName("subject");
+        $objPost->setStrGuestbookPostText("test");
         $objPost->updateObjectToDb($strGBId);
 
         $objDB->flushQueryCache();
 
-        $this->assertEquals(0, count(class_modul_guestbook_post::getPosts($strGBId, true)), __FILE__." check nr of posts portal");
-        $this->assertEquals(1, count(class_modul_guestbook_post::getPosts($strGBId)), __FILE__." check nr of posts total");
+        $this->assertEquals(0, count(class_module_guestbook_post::getPosts($strGBId, true)), __FILE__." check nr of posts portal");
+        $this->assertEquals(1, count(class_module_guestbook_post::getPosts($strGBId)), __FILE__." check nr of posts total");
 
         echo "setting guestbook non-moderated...\n";
-        $objGuestbook->setGuestbookModerated(1);
+        $objGuestbook->setIntGuestbookModerated(0);
         $objGuestbook->updateObjectToDb();
 
 
         $objDB->flushQueryCache();
 
         echo "adding a new post...\n";
-        $objPost = new class_modul_guestbook_post();
-        $objPost->setGuestbookPostName("subject2");
-        $objPost->setGuestbookPostText("test2");
+        $objPost = new class_module_guestbook_post();
+        $objPost->setStrGuestbookPostName("subject2");
+        $objPost->setStrGuestbookPostText("test2");
         $objPost->updateObjectToDb($strGBId);
 
-        $this->assertEquals(1, count(class_modul_guestbook_post::getPosts($strGBId, true)), __FILE__." check nr of posts portal");
-        $this->assertEquals(2, count(class_modul_guestbook_post::getPosts($strGBId)), __FILE__." check nr of posts total");
+        $this->assertEquals(1, count(class_module_guestbook_post::getPosts($strGBId, true)), __FILE__." check nr of posts portal");
+        $this->assertEquals(2, count(class_module_guestbook_post::getPosts($strGBId)), __FILE__." check nr of posts total");
 
 
         $objDB->flushQueryCache();
 
         echo "deleting the guestbook...\n";
-        $objGuestbook->deleteGuestbook();
+        $objGuestbook->deleteObject();
 
        
     }
