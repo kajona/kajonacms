247c652222232b38a93dad34411c0a6d05fd10a1
sidler <sidler@mulchprod.de>
2012-04-17 16:30:30 +0000

    added missing lang-entries

--- module_guestbook/lang/module_elements/lang_guestbook_ru.php
+++ module_guestbook/lang/module_elements/lang_guestbook_ru.php
@@ -1,14 +1,15 @@
 <?php
 /*"******************************************************************************************************
 *   (c) 2004-2006 by MulchProductions, www.mulchprod.de                                                 *
-*   (c) 2007-2011 by Kajona, www.kajona.de                                                              *
+*   (c) 2007-2012 by Kajona, www.kajona.de                                                              *
 *       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
 *-------------------------------------------------------------------------------------------------------*
-*	$Id: lang_guestbook_ru.php 3949 2011-06-25 10:46:38Z sidler $					    *
+*	$Id$					    *
 ********************************************************************************************************/
 //Edited with Kajona Language Editor GUI, see www.kajona.de and www.mulchprod.de for more information
-//Kajona Language Editor Core Build 337
+//Kajona Language Editor Core Build 363
 
 //editable entries
+$lang["element_guestbook_name"]          = "Гостевая книга";
 $lang["guestbook_amount"]                = "Количество постов на странице:";
 $lang["guestbook_id"]                    = "Гостевая книга:";
