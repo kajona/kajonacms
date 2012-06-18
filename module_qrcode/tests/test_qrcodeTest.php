fc8496ff9c16199dd82ae7a507110a1b1d336e86
mrbashshell <bash@stefanbongartz.de>
2012-06-18 14:22:02 +0000

    NEW: module to generate QR Codes

---
+++ module_qrcode/tests/test_qrcodeTest.php
@@ -0,0 +1,24 @@
+<?php
+require_once (__DIR__ . "/../../module_system/system/class_testbase.php");
+
+class class_test_qrcode extends class_testbase  {
+
+    public function testQrcode() {
+
+
+        $objQrCode = new class_qrcode();
+
+        $strImage1 = $objQrCode->getImageForString("Kajona Test Image");
+        $this->assertFileExists(_realpath_.$strImage1);
+
+        $strImage2 = $objQrCode->getImageForString(_webpath_);
+        $this->assertFileExists(_realpath_.$strImage2);
+
+
+        echo "\t <img src=\""._webpath_.$strImage1."\" />\n";
+        echo "\t <img src=\""._webpath_.$strImage2."\" />\n";
+
+    }
+
+
+}
