7bfe702a1c96774db73cceeba14c955e11724027
sidler <sidler@mulchprod.de>
2012-10-14 14:29:11 +0000

    removed senseless die()-exit

--- module_mediamanager/download.php
+++ module_mediamanager/download.php
@@ -21,89 +21,90 @@ class class_download_manager extends class_root {
      *
      * @param $strSystemid
      */
-	public function __construct($strSystemid) {
-	    parent::__construct($strSystemid);
+    public function __construct($strSystemid) {
+        parent::__construct($strSystemid);
 
         //Increase max execution time
         @ini_set("max_execution_time", "7200");
-	}
+    }
 
-	/**
-	 * Sends the requested file to the browser
-	 *
-	 */
+    /**
+     * Sends the requested file to the browser
+
+     */
     public function actionDownload() {
-		//Load filedetails
+        //Load filedetails
 
-		$bitRedirectToErrorPage = false;
+        $bitRedirectToErrorPage = false;
 
-		if(validateSystemid($this->getSystemid())) {
+        if(validateSystemid($this->getSystemid())) {
 
             /** @var $objFile class_module_mediamanager_file */
-		    $objFile = class_objectfactory::getInstance()->getObject($this->getSystemid());
-			//Succeeded?
-			if($objFile instanceof class_module_mediamanager_file && $objFile->getStatus() == "1" && $objFile->getIntType() == class_module_mediamanager_file::$INT_TYPE_FILE) {
-				//Check rights
-				if($objFile->rightRight2()) {
-				    //Log the download
-					class_module_mediamanager_logbook::generateDlLog($objFile);
-					//Send the data to the browser
-					$strBrowser = getServer("HTTP_USER_AGENT");
-					//Check the current browsertype
-					if(uniStrpos($strBrowser, "IE") !== false) {
-						//Internet Explorer
+            $objFile = class_objectfactory::getInstance()->getObject($this->getSystemid());
+            //Succeeded?
+            if($objFile instanceof class_module_mediamanager_file && $objFile->getStatus() == "1" && $objFile->getIntType() == class_module_mediamanager_file::$INT_TYPE_FILE) {
+                //Check rights
+                if($objFile->rightRight2()) {
+                    //Log the download
+                    class_module_mediamanager_logbook::generateDlLog($objFile);
+                    //Send the data to the browser
+                    $strBrowser = getServer("HTTP_USER_AGENT");
+                    //Check the current browsertype
+                    if(uniStrpos($strBrowser, "IE") !== false) {
+                        //Internet Explorer
                         class_response_object::getInstance()->addHeader("Content-type: application/x-ms-download");
                         class_response_object::getInstance()->addHeader("Content-type: x-type/subtype\n");
                         class_response_object::getInstance()->addHeader("Content-type: application/force-download");
-                        class_response_object::getInstance()->addHeader("Content-Disposition: attachment; filename=".preg_replace(
-                            '/\./', '%2e',
-                            saveUrlEncode(trim(basename($objFile->getStrFilename()))), substr_count(basename($objFile->getStrFilename()), '.') - 1
-                        ));
-					}
-					else {
-						//Good: another browser vendor
+                        class_response_object::getInstance()->addHeader(
+                            "Content-Disposition: attachment; filename=" . preg_replace(
+                                '/\./', '%2e',
+                                saveUrlEncode(trim(basename($objFile->getStrFilename()))), substr_count(basename($objFile->getStrFilename()), '.') - 1
+                            )
+                        );
+                    }
+                    else {
+                        //Good: another browser vendor
                         class_response_object::getInstance()->addHeader("Content-Type: application/octet-stream");
-                        class_response_object::getInstance()->addHeader("Content-Disposition: attachment; filename=".saveUrlEncode(trim(basename($objFile->getStrFilename()))));
-					}
-					//Common headers
+                        class_response_object::getInstance()->addHeader("Content-Disposition: attachment; filename=" . saveUrlEncode(trim(basename($objFile->getStrFilename()))));
+                    }
+                    //Common headers
                     class_response_object::getInstance()->addHeader("Expires: Mon, 01 Jan 1995 00:00:00 GMT");
-                    class_response_object::getInstance()->addHeader("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+                    class_response_object::getInstance()->addHeader("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
                     class_response_object::getInstance()->addHeader("Pragma: no-cache");
                     class_response_object::getInstance()->addHeader("Content-description: JustThum-Generated Data\n");
-                    class_response_object::getInstance()->addHeader("Content-Length: ".filesize(_realpath_.$objFile->getStrFilename()));
+                    class_response_object::getInstance()->addHeader("Content-Length: " . filesize(_realpath_ . $objFile->getStrFilename()));
 
-					//End Session
-					$this->objSession->sessionClose();
+                    //End Session
+                    $this->objSession->sessionClose();
                     class_response_object::getInstance()->sendHeaders();
 
-					//Loop the file
-					$ptrFile = @fopen(_realpath_.$objFile->getStrFilename(), 'rb');
+                    //Loop the file
+                    $ptrFile = @fopen(_realpath_ . $objFile->getStrFilename(), 'rb');
                     fpassthru($ptrFile);
-					@fclose($ptrFile);
+                    @fclose($ptrFile);
 
-                    die();
-				}
-				else {
+                }
+                else {
                     class_response_object::getInstance()->setStrStatusCode(class_http_statuscodes::SC_FORBIDDEN);
                     $bitRedirectToErrorPage = true;
-				}
+                }
 
-			}
-			else {
+            }
+            else {
                 class_response_object::getInstance()->setStrStatusCode(class_http_statuscodes::SC_NOT_FOUND);
-				$bitRedirectToErrorPage = true;
-			}
+                $bitRedirectToErrorPage = true;
+            }
 
-		}
-		else {
+        }
+        else {
             class_response_object::getInstance()->setStrStatusCode(class_http_statuscodes::SC_NOT_FOUND);
             $bitRedirectToErrorPage = true;
-		}
+        }
 
-		if($bitRedirectToErrorPage) {
+        if($bitRedirectToErrorPage) {
             class_response_object::getInstance()->setStrRedirectUrl(str_replace(array("_indexpath_", "&amp;"), array(_indexpath_, "&"), getLinkPortalHref(_pages_errorpage_)));
-		}
-	}
+        }
+    }
 }
 
 
