b4d5e7385a2b7d1a60aca32dbdb006c9f050bdb6
sidler <sidler@mulchprod.de>
2012-11-21 17:29:17 +0000

    activated the kajona.de contentprovider, no longer mapped against localhost

--- module_packagemanager/system/class_module_packagemanager_contentprovider_kajona.php
+++ module_packagemanager/system/class_module_packagemanager_contentprovider_kajona.php
@@ -18,17 +18,12 @@
 class class_module_packagemanager_contentprovider_kajona extends class_module_packagemanager_contentprovider_remote_base {
 
     function __construct() {
-        //TODO: replace with kajona url
-        $strBrowseHost = "";
-        $strBrowseUrl   = "";
-        $strDownloadUrl = "";
-
-        if(isset($_SERVER["HTTP_HOST"])) {
-            $strBrowseHost  = $_SERVER["HTTP_HOST"];
-            $strBrowseUrl   = str_replace("http://".$_SERVER["HTTP_HOST"], "", _webpath_)."/xml.php?module=packageserver&action=list";
-            $strDownloadUrl = str_replace("http://".$_SERVER["HTTP_HOST"], "", _webpath_)."/download.php";
-        }
-
-        parent::__construct("provider_kajona", $strBrowseHost, $strBrowseUrl, $strDownloadUrl, __CLASS__);
+        parent::__construct(
+            "provider_kajona",
+            "www.kajona.de",
+            "/xml.php?module=packageserver&action=list",
+            "/download.php",
+            __CLASS__
+        );
     }
 }
