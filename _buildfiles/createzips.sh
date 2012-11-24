1d40964e1ccff25cdfbf97acaa512dbdfb412e73
sidler <sidler@mulchprod.de>
2012-11-24 14:48:21 +0000

    added a shell script in order to create zips out of every core-module / element.

---
+++ _buildfiles/createzips.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+
+createModuleZips() 
+{ 
+  echo "deleting target folder packages...";
+  if [ -d "$PWD/packages" ]
+  then
+    rm -rf "$PWD/packages"
+  fi
+  mkdir "$PWD/packages"
+
+  echo "creating single zips folders...";
+  
+  for i in `ls -a "$PWD/temp/kajona/core"`
+  do
+    if [ -d "$PWD/temp/kajona/core/$i" ] && [ $i != ".svn" ] && [ $i != "packages" ] && [ $i != "kajona" ] && [ $i != "." ] && [ $i != ".." ]
+    then
+      echo $i
+      cd "temp/kajona/core/$i/"
+      zip -r -p "../../../../packages/$i" "."
+      cd "../../../../" 
+
+    fi
+  done
+  
+}
+
+
+echo "Packaging...\n";
+
+echo "calling the ant task modulePackagesZipHelper\n";
+ant -f $PWD/build_jenkins.xml modulePackagesZipHelper
+#createModuleZips
+
