3cb69637ceb9261279ccfc9af1155d847e40a822
sidler <sidler@mulchprod.de>
2012-11-26 15:27:00 +0000

    Updated parts of the documentation, removed trivial module documentations

--- module_rating/system/class_module_rating_algo_absolute.php
+++ module_rating/system/class_module_rating_algo_absolute.php
@@ -9,25 +9,26 @@
 
 /**
  * Does an absolute, linear rating based on the current rating-value
+ *
  * @package module_rating
  */
 class class_module_rating_algo_absolute implements interface_module_rating_algo {
-	
-	
-	/**
+
+
+    /**
      * Calculates the new rating
-     * 
+     *
      * @param class_module_rating_rate $objSourceRate The rating-record to update
      * @param float $floatNewRating The rating fired by the user
+     *
      * @return float the new rating
      */
     public function doRating(class_module_rating_rate $objSourceRate, $floatNewRating) {
-    	//calc the new rating
-        $floatNewRating = (($objSourceRate->getFloatRating() * $objSourceRate->getIntHits()) + $floatNewRating) / ($objSourceRate->getIntHits()+1);
-        
+        //calc the new rating
+        $floatNewRating = (($objSourceRate->getFloatRating() * $objSourceRate->getIntHits()) + $floatNewRating) / ($objSourceRate->getIntHits() + 1);
+
         return $floatNewRating;
     }
-	
-	
+
 }
 
