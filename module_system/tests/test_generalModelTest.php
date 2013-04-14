c3826410476e685ca3042f8861321f1d422bf5b2
sidler <sidler@mulchprod.de>
2013-04-14 12:00:55 +0000

    updated unittest

--- module_system/tests/test_generalModelTest.php
+++ module_system/tests/test_generalModelTest.php
@@ -36,7 +36,8 @@ class class_test_generalModelTest extends class_testbase {
                             "class_module_user_group",
                             "class_module_user_user",
                             "class_module_workflows_workflow",
-                            "class_module_pages_pageelement"
+                            "class_module_pages_pageelement",
+                            "class_module_stats_worker"
                         )
                     )
                     ) {
