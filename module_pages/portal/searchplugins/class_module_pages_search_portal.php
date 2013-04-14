73db4a886aa24165e9f6da844a2d2c2a5d5c7466
sidler <sidler@mulchprod.de>
2013-04-14 09:59:09 +0000

    code style

--- module_pages/portal/searchplugins/class_module_pages_search_portal.php
+++ module_pages/portal/searchplugins/class_module_pages_search_portal.php
@@ -12,13 +12,12 @@
  * Search plugin of the pages-module. Searches the configured page-elements and the pages-data.
  * To add page-elements written on your own, create the appropriate array-entries.
  * In detail: Create a row for each table-row, you want to search
- *
  * e.g: $arrSearch["pages_elements"]["table_to_search"][] = "row_to_search"
  *
  * @package module_pages
  * @author sidler@mulchprod.de
  */
-class class_module_pages_search_portal implements interface_search_plugin  {
+class class_module_pages_search_portal implements interface_search_plugin {
 
     private $strSearchterm = "";
 
@@ -48,14 +47,13 @@ class class_module_pages_search_portal implements interface_search_plugin  {
     }
 
 
-
     /**
      * searches the pages for the given term
      *
      * @return void
      * @internal param mixed $arrTableConfig
      */
-	private function searchPages() {
+    private function searchPages() {
         $objLanguages = new class_module_languages_language();
 
         $arrWhere = array(
@@ -66,23 +64,23 @@ class class_module_pages_search_portal implements interface_search_plugin  {
         );
         $arrParams = array(
             $objLanguages->getStrPortalLanguage(),
-            "%".$this->strSearchterm."%",
-            "%".$this->strSearchterm."%",
-            "%".$this->strSearchterm."%",
-            "%".$this->strSearchterm."%"
+            "%" . $this->strSearchterm . "%",
+            "%" . $this->strSearchterm . "%",
+            "%" . $this->strSearchterm . "%",
+            "%" . $this->strSearchterm . "%"
         );
-        $strWhere = "( ".implode(" OR ", $arrWhere). " ) ";
+        $strWhere = "( " . implode(" OR ", $arrWhere) . " ) ";
 
         //build query
         $strQuery = "SELECT page_name, pageproperties_browsername, pageproperties_description, page_id
-                     FROM "._dbprefix_."page,
-                          "._dbprefix_."page_properties,
-                          "._dbprefix_."system
+                     FROM " . _dbprefix_ . "page,
+                          " . _dbprefix_ . "page_properties,
+                          " . _dbprefix_ . "system
                      WHERE pageproperties_language = ?
                        AND pageproperties_id = page_id
                        AND system_id = page_id
                        AND system_status = 1
-                       AND   ".$strWhere."
+                       AND   " . $strWhere . "
                      ORDER BY system_sort ASC";
 
         $arrPages = $this->objDB->getPArray($strQuery, $arrParams);
@@ -94,14 +92,14 @@ class class_module_pages_search_portal implements interface_search_plugin  {
                 if($arrOnePage["page_name"] != "master") {
                     if(isset($this->arrHits[$arrOnePage["page_name"]])) {
                         $objResult = $this->arrHits[$arrOnePage["page_name"]];
-                        $objResult->setIntHits($objResult->getIntHits()+1);
+                        $objResult->setIntHits($objResult->getIntHits() + 1);
                     }
                     else {
                         $strText = $arrOnePage["pageproperties_browsername"] != "" ? $arrOnePage["pageproperties_browsername"] : $arrOnePage["page_name"];
                         $objResult = new class_search_result();
                         $objResult->setStrResultId($arrOnePage["page_id"]);
                         $objResult->setStrSystemid($arrOnePage["page_id"]);
-                        $objResult->setStrPagelink(getLinkPortal($arrOnePage["page_name"], "", "_self", $strText, "", "&highlight=".urlencode(html_entity_decode($this->strSearchterm, ENT_QUOTES, "UTF-8"))));
+                        $objResult->setStrPagelink(getLinkPortal($arrOnePage["page_name"], "", "_self", $strText, "", "&highlight=" . urlencode(html_entity_decode($this->strSearchterm, ENT_QUOTES, "UTF-8"))));
                         $objResult->setStrPagename($arrOnePage["page_name"]);
                         $objResult->setStrDescription($arrOnePage["pageproperties_description"]);
 
@@ -110,7 +108,7 @@ class class_module_pages_search_portal implements interface_search_plugin  {
                 }
             }
         }
-	}
+    }
 
     private function searchPageTags() {
         if(class_module_system_module::getModuleByName("tags") != null) {
@@ -123,12 +121,12 @@ class class_module_pages_search_portal implements interface_search_plugin  {
             );
 
             $strQuery = "SELECT page_name, pageproperties_browsername, pageproperties_description, page_id
-                          FROM "._dbprefix_."system,
-                               "._dbprefix_."tags_member,
-                               "._dbprefix_."tags_tag,
-                               "._dbprefix_."page_properties,
-                               "._dbprefix_."page
-                         WHERE system_module_nr = "._pages_modul_id_."
+                          FROM " . _dbprefix_ . "system,
+                               " . _dbprefix_ . "tags_member,
+                               " . _dbprefix_ . "tags_tag,
+                               " . _dbprefix_ . "page_properties,
+                               " . _dbprefix_ . "page
+                         WHERE system_module_nr = " . _pages_modul_id_ . "
                            AND pageproperties_language = ?
 						   AND pageproperties_id = page_id
 						   AND system_id = page_id
@@ -141,30 +139,30 @@ class class_module_pages_search_portal implements interface_search_plugin  {
             $arrPages = $this->objDB->getPArray($strQuery, $arrParams);
 
             //register the found pages
-			if(count($arrPages) > 0) {
-				foreach($arrPages as $arrOnePage) {
-					//Dont find the master-page!!!
-					if($arrOnePage["page_name"] != "master") {
-						if(isset($this->arrHits[$arrOnePage["page_name"]])) {
+            if(count($arrPages) > 0) {
+                foreach($arrPages as $arrOnePage) {
+                    //Dont find the master-page!!!
+                    if($arrOnePage["page_name"] != "master") {
+                        if(isset($this->arrHits[$arrOnePage["page_name"]])) {
                             $objResult = $this->arrHits[$arrOnePage["page_name"]];
-                            $objResult->setIntHits($objResult->getIntHits()+1);
-    					}
-    					else {
+                            $objResult->setIntHits($objResult->getIntHits() + 1);
+                        }
+                        else {
                             $strText = $arrOnePage["pageproperties_browsername"] != "" ? $arrOnePage["pageproperties_browsername"] : $arrOnePage["page_name"];
                             $objResult = new class_search_result();
                             $objResult->setStrResultId($arrOnePage["page_id"]);
                             $objResult->setStrSystemid($arrOnePage["page_id"]);
-                            $objResult->setStrPagelink(getLinkPortal($arrOnePage["page_name"], "", "_self", $strText, "", "&highlight=".urlencode(html_entity_decode($this->strSearchterm, ENT_QUOTES, "UTF-8"))));
+                            $objResult->setStrPagelink(getLinkPortal($arrOnePage["page_name"], "", "_self", $strText, "", "&highlight=" . urlencode(html_entity_decode($this->strSearchterm, ENT_QUOTES, "UTF-8"))));
                             $objResult->setStrPagename($arrOnePage["page_name"]);
                             $objResult->setStrDescription($arrOnePage["pageproperties_description"]);
 
                             $this->arrHits[$arrOnePage["page_name"]] = $objResult;
-    					}
-					}
-				}
-			}
+                        }
+                    }
+                }
+            }
 
-       }
+        }
     }
 
 }
