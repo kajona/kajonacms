08b5710a3609700ddca851f689b4c1c438623c42
sidler <sidler@mulchprod.de>
2013-06-30 17:44:25 +0000

    added postgres build files

---
+++ _buildfiles/config_postgres.php
@@ -0,0 +1,16 @@
+<?php 
+
+//database-settings
+define("DB_HOST",				                "localhost");
+define("DB_USER",                               "kajona_v4");
+define("DB_PASS",                               "kajona_v4");
+define("DB_DB",                                 "kajona_v4");
+//define("DB_DRIVER",                             "oci8");
+//define("DB_DRIVER",                             "mysqli");
+define("DB_DRIVER",                             "postgres");
+
+
+
+
+ini_set("session.save_path", "/tmp");
+ini_set("session.use_cookies", "Off");
