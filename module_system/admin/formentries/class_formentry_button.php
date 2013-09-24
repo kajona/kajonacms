3fa1294a48c605dc23533c62c90454c5521939f4
sidler <sidler@mulchprod.de>
2013-09-24 14:34:33 +0000

    NEW: added a button-formentry. does what it says :)

---
+++ module_system/admin/formentries/class_formentry_button.php
@@ -0,0 +1,42 @@
+<?php
+/*"******************************************************************************************************
+*   (c) 2007-2013 by Kajona, www.kajona.de                                                              *
+*       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
+*-------------------------------------------------------------------------------------------------------*
+*	$Id$                               *
+********************************************************************************************************/
+
+/**
+ * @author sidler@mulchprod.de
+ * @since 4.3
+ * @package module_formgenerator
+ */
+class class_formentry_button extends class_formentry_base implements interface_formentry {
+
+
+    public function __construct($strFormName, $strSourceProperty = "", class_model $objSourceObject = null) {
+        parent::__construct($strFormName, $strSourceProperty, $objSourceObject);
+
+        //set the default validator
+        $this->setObjValidator(new class_dummy_validator());
+    }
+
+    /**
+     * Renders the field itself.
+     * In most cases, based on the current toolkit.
+     *
+     * @return string
+     */
+    public function renderField() {
+        $objToolkit = class_carrier::getInstance()->getObjToolkit("admin");
+        $strReturn = "";
+        if($this->getStrHint() != null)
+            $strReturn .= $objToolkit->formTextRow($this->getStrHint());
+
+        $strReturn .= $objToolkit->formInputSubmit($this->getStrLabel(), $this->getStrValue(), "", $this->getBitReadonly());
+
+        return $strReturn;
+    }
+
+
+}
