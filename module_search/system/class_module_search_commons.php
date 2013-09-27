2d88066e10937f276ff8d9b1ce51a23db594cabd
sidler <sidler@mulchprod.de>
2013-09-27 11:59:04 +0000

    BUG: fixed phpdoc

--- module_search/system/class_module_search_commons.php
+++ module_search/system/class_module_search_commons.php
@@ -65,7 +65,7 @@ class class_module_search_commons extends class_model implements interface_model
 
     /**
      * Calls the single search-functions, sorts the results and creates the output.
-     * Method for portal-searches.
+     * Method for backend-searches.
      *
      * @param $objSearch class_module_search_search
      *
