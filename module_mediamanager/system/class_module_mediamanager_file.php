9c7d83a991298588bfe1839049bde6a63e0bea7a
spoonguard2k <stefan.meyer1@yahoo.de>
2013-10-23 15:09:56 +0000

    BUG: Updated a SQL statement for searching a package within the database

--- module_mediamanager/system/class_module_mediamanager_file.php
+++ module_mediamanager/system/class_module_mediamanager_file.php
@@ -314,7 +314,7 @@ class class_module_mediamanager_file extends class_model implements interface_mo
                 $strWhere = "AND ( ".implode(" OR ", $arrWhere)." )";
             }
             else {
-                $arrParams[] = $strNameFilter."%";
+                $arrParams[] = "%".$strNameFilter.".zip";
                 $strWhere = "AND file_name LIKE ?";
             }
         }
@@ -366,7 +366,7 @@ class class_module_mediamanager_file extends class_model implements interface_mo
                 $strWhere = "AND ( ".implode(" OR ", $arrWhere)." )";
             }
             else {
-                $arrParams[] = $strNameFilter."%";
+                $arrParams[] = "%".$strNameFilter.".zip";
                 $strWhere = "AND file_name LIKE ?";
             }
         }
