426366c71003a51f924e02419678e01f0d1ad670
sidler <sidler@mulchprod.de>
2013-12-20 08:01:13 +0000

    Removed some old permission language entries

--- module_tags/lang/module_tags/lang_tags_sv.php
+++ module_tags/lang/module_tags/lang_tags_sv.php
@@ -12,7 +12,6 @@
 //editable entries
 $lang["button_add"]                      = "Infoga";
 $lang["delete_question"]                 = "Vill du verkligen radera taggen &quot;<b>%%element_name%%</b>&quot;?";
-$lang["error_permissions"]               = "Inte tillräckliga rättigheter för att utföra denna handling";
 $lang["form_tags_name"]                  = "Tagnamn";
 $lang["modul_rechte"]                    = "Modulrättigheter";
 $lang["modul_titel"]                     = "Tags";
