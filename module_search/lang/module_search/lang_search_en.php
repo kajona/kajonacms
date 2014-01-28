17c444b751b53a8778b77cd6f4b3c094891d85eb
mrbashshell <bash@stefanbongartz.de>
2014-01-28 08:17:49 +0000

    NEW: added quickhelps

--- module_search/lang/module_search/lang_search_en.php
+++ module_search/lang/module_search/lang_search_en.php
@@ -33,3 +33,6 @@ $lang["stats_title"]                     = "Searchqueries";
 $lang["submit_label"]                    = "Search";
 $lang["systemtask_search_indexrebuild"]              = "Rebuild search index";
 $lang["worker_indexrebuild_end"]              = "Index was rebuilt successfully.<br /><br />Number of documents in index: {0}<br />Number of entries in index: {1}";
+$lang["quickhelp_list"]                  = "This list encompasses search queries created and saved by the respective user (e.g. saved due to regular queries). These search queries can be further detailed / parametrized or started from here.";
+$lang["quickhelp_search"]               = "The search query provides the ability to check the entire database for a specific keyword. Therefore the desired search term is entered and the search is triggered by clicking the corresponding button. The respective results are displayed in list form and can be edited according to user rights.";
+$lang["quickhelp_new"]                  = "By using this function it is possible to create regular/continuous search queries. By clicking the corresponding button information will be saved to the search list and the query can be specified, started or changed.";
