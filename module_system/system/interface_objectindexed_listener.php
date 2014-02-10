3fb380848afb0b0ea6eed23dacd2e46fdc63f465
sidler <sidler@mulchprod.de>
2014-02-10 14:33:08 +0000

    BUG: moved the object-indexed interface to module system. otherwise the class-loader would crash if module search is not present. the problem behind is addressed in ticket #876.

--- module_system/system/interface_objectindexed_listener.php
+++ module_system/system/interface_objectindexed_listener.php
@@ -8,7 +8,7 @@
 
 
 /**
- * If an object is added/updated to the search index, an event is throws.
+ * If an object is added/updated to the search index, an event is thrown.
  * This allows listeners to add special content / keywords to the index-document.
  *
  * @author sidler@mulchprod.de
@@ -18,14 +18,13 @@
 interface interface_objectindexed_listener {
 
     /**
-     * Called whenever an object is index and to be added to the search-index.
+     * Called whenever an object is indexed and to be added to the search-index.
      * Use this callback to add additional content to the objects search-document.
      *
-     * @abstract
-     *
-     * @param $objObject
+     * @param class_root $objObject
      * @param class_module_search_document $objSearchDocument
      *
+     * @abstract
      * @return bool
      */
     public function handleObjectIndexedEvent($objObject, class_module_search_document $objSearchDocument);
