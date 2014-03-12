805cbbb060f79e892d3ba0e451878b263cd67344
mrbashshell <bash@stefanbongartz.de>
2014-03-12 14:17:25 +0000

    UPDATE: formated all langfiles

--- module_tags/lang/module_tags/lang_tags_de.php
+++ module_tags/lang/module_tags/lang_tags_de.php
@@ -7,10 +7,11 @@
 *	$Id$					    *
 ********************************************************************************************************/
 //Edited with Kajona Language Editor GUI, see www.kajona.de and www.mulchprod.de for more information
-//Kajona Language Editor Core Build 395
+//Kajona Language Editor Core Build 398
 
 //non-editable entries
 $lang["permissions_header"]              = array(0 => "Anzeigen", 1 => "Bearbeiten", 2 => "Löschen", 3 => "Rechte", 4 => "Favoriten", 5 => "", 6 => "", 7 => "", 8 => "");
+
 
 //editable entries
 $lang["_tags_defaultprivate_"]           = "Zuweisungen standardmäßig privat";
@@ -21,20 +22,18 @@ $lang["adminskin_tooltip"]               = "Favoriten anzeigen";
 $lang["button_add"]                      = "Hinzufügen";
 $lang["delete_question"]                 = "Möchten Sie den Tag &quot;<b>%%element_name%%</b>&quot; wirklich löschen?";
 $lang["delete_question_fav"]             = "Möchten Sie den Favoriten &quot;<b>%%element_name%%</b>&quot; wirklich löschen?";
-$lang["form_tags_name"]                  = "Tag-Name";
-$lang["form_tags_private"]               = "Private Zuweisungen";
-$lang["modul_titel"]                     = "Tags";
-$lang["required_tag_name"]               = "Tag-Name";
-$lang["tag_assignments"]                 = "Zuweisungen";
-$lang["tag_edit"]                        = "Bearbeiten";
-$lang["tag_name_hint"]                   = "Mehrere Tags können in Form einer kommaseparierten Liste eingegeben werden";
-$lang["tagsection_header"]               = "Tags";
-
-
 $lang["favorite_save_error"]             = "Fehler beim Speichern des Favoriten";
 $lang["favorite_save_remove"]            = "Der Favorit wurde gelöscht";
 $lang["favorite_save_success"]           = "Der Favorit wurde angelegt";
+$lang["form_tags_name"]                  = "Tag-Name";
+$lang["form_tags_private"]               = "Private Zuweisungen";
+$lang["modul_titel"]                     = "Tags";
+$lang["quickhelp_list"]                  = "Tags bieten einem Benutzer die Möglichkeit, häufig verwendete Sachverhalte effizient wieder zu finden. Auf verschiedenen Ebenen des Portals können Tags angelegt werden (z.B. einzelne Bewertungen, bestimmte To Dos), die dann im Modul „Tag“ dargestellt werden. n dieser Liste werden alle angelegten Tags eines Benutzers angezeigt. Diese können hier angezeigt und weiter bearbeitet werden. Datensätze, die mit einem Tag versehen wurden können über den Tag schnell aufgelistet werden. Tags dienen also als Schlagworte und zur Gruppierung von Datensätzen.";
+$lang["quickhelp_list_favorites"]        = "Für einen schnellen Zugriff auf Tags können diese als favorisiert markiert werden. Favorisierte Tags werden im Benutzer-Menü links oben für einen direkten Zugriff angezeigt. ";
+$lang["required_tag_name"]               = "Tag-Name";
+$lang["tag_assignments"]                 = "Zuweisungen";
+$lang["tag_edit"]                        = "Bearbeiten";
 $lang["tag_favorite_add"]                = "Als Favorit setzen";
 $lang["tag_favorite_remove"]             = "Favorit löschen";
-$lang["quickhelp_list"]                  = "Tags bieten einem Benutzer die Möglichkeit, häufig verwendete Sachverhalte effizient wieder zu finden. Auf verschiedenen Ebenen des Portals können Tags angelegt werden (z.B. einzelne Bewertungen, bestimmte To Dos), die dann im Modul „Tag“ dargestellt werden. n dieser Liste werden alle angelegten Tags eines Benutzers angezeigt. Diese können hier angezeigt und weiter bearbeitet werden. Datensätze, die mit einem Tag versehen wurden können über den Tag schnell aufgelistet werden. Tags dienen also als Schlagworte und zur Gruppierung von Datensätzen.";
-$lang["quickhelp_list_favorites"]         = "Für einen schnellen Zugriff auf Tags können diese als favorisiert markiert werden. Favorisierte Tags werden im Benutzer-Menü links oben für einen direkten Zugriff angezeigt. ";
+$lang["tag_name_hint"]                   = "Mehrere Tags können in Form einer kommaseparierten Liste eingegeben werden";
+$lang["tagsection_header"]               = "Tags";
--- module_tags/lang/module_tags/lang_tags_en.php
+++ module_tags/lang/module_tags/lang_tags_en.php
@@ -7,10 +7,11 @@
 *	$Id$					    *
 ********************************************************************************************************/
 //Edited with Kajona Language Editor GUI, see www.kajona.de and www.mulchprod.de for more information
-//Kajona Language Editor Core Build 395
+//Kajona Language Editor Core Build 398
 
 //non-editable entries
 $lang["permissions_header"]              = array(0 => "View", 1 => "Edit", 2 => "Delete", 3 => "Permissions", 4 => "Favorites",  5 => "",	6 => "", 7 => "", 8 => "" );
+
 
 //editable entries
 $lang["_tags_defaultprivate_"]           = "Private assignments by default";
@@ -21,19 +22,18 @@ $lang["adminskin_tooltip"]               = "Show favorites";
 $lang["button_add"]                      = "Add";
 $lang["delete_question"]                 = "Do you really want to delete the tag &quot;<b>%%element_name%%</b>&quot;?";
 $lang["delete_question_fav"]             = "Do you really want to delete the favorite &quot;<b>%%element_name%%</b>&quot;?";
-$lang["form_tags_name"]                  = "Tag-Name";
-$lang["form_tags_private"]               = "Private assignments";
-$lang["modul_titel"]                     = "Tags";
-$lang["required_tag_name"]               = "Tag-Name";
-$lang["tag_assignments"]                 = "Assignments";
-$lang["tag_edit"]                        = "Edit";
-$lang["tag_name_hint"]                   = "A set of tags may be submitted as a comma-separated list";
-$lang["tagsection_header"]               = "Tags";
-
 $lang["favorite_save_error"]             = "An error occurred while saving favorite";
 $lang["favorite_save_remove"]            = "Favorite has been deleted";
 $lang["favorite_save_success"]           = "Favorite has been saved successfully.";
+$lang["form_tags_name"]                  = "Tag-Name";
+$lang["form_tags_private"]               = "Private assignments";
+$lang["modul_titel"]                     = "Tags";
+$lang["quickhelp_list"]                  = "Tags provide the ability for users to find frequently used facts efficiently. Tags can be created on different levels of the software (e.g. individual evaluations, specific to-dos), which are displayed in the module ‘Tag’. Within this list all tags of a user are displayed. These tags can be displayed and edited here. Records assigned to a tag are listable through the tag. In consequence, tags may be used to add keywords to a record or to create groups of logically connected records.";
+$lang["quickhelp_list_favorites"]        = "Important tags may be marked as favorite tag. Those tags will be included in the user-menu at the upper left corner, providing a fast access to the tagged objects.";
+$lang["required_tag_name"]               = "Tag-Name";
+$lang["tag_assignments"]                 = "Assignments";
+$lang["tag_edit"]                        = "Edit";
 $lang["tag_favorite_add"]                = "Set as favorite";
 $lang["tag_favorite_remove"]             = "Delete favorite";
-$lang["quickhelp_list"]                  = "Tags provide the ability for users to find frequently used facts efficiently. Tags can be created on different levels of the software (e.g. individual evaluations, specific to-dos), which are displayed in the module ‘Tag’. Within this list all tags of a user are displayed. These tags can be displayed and edited here. Records assigned to a tag are listable through the tag. In consequence, tags may be used to add keywords to a record or to create groups of logically connected records.";
-$lang["quickhelp_list_favorites"]         = "Important tags may be marked as favorite tag. Those tags will be included in the user-menu at the upper left corner, providing a fast access to the tagged objects.";
+$lang["tag_name_hint"]                   = "A set of tags may be submitted as a comma-separated list";
+$lang["tagsection_header"]               = "Tags";
