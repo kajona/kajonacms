d158a52b1e77f178f9deafa0a1c61a42635fa810
sidler <sidler@mulchprod.de>
2014-04-11 11:10:27 +0000

    BUG: fixed the zip-unittest since the licence file moved

--- module_system/tests/test_zipTest.php
+++ module_system/tests/test_zipTest.php
@@ -15,8 +15,8 @@ class class_test_zip extends class_testbase  {
         echo "\topening "._realpath_."/test.zip\n";
         $this->assertTrue($objZip->openArchiveForWriting("/files/cache/test.zip"), __FILE__." openArchive");
 
-        $this->assertTrue($objZip->addFile(class_resourceloader::getInstance()->getCorePathForModule("module_system")."/module_system/system/licence_lgpl.txt"), __FILE__." addFile");
-        $this->assertTrue($objZip->addFile(class_resourceloader::getInstance()->getCorePathForModule("module_system")."/module_system/system/licence_lgpl.txt", "/lgpl_plain.txt"), __FILE__." addFileRenamed");
+        $this->assertTrue($objZip->addFile(class_resourceloader::getInstance()->getCorePathForModule("module_system")."/module_system/metadata.xml"), __FILE__." addFile");
+        $this->assertTrue($objZip->addFile(class_resourceloader::getInstance()->getCorePathForModule("module_system")."/module_system/metadata.xml", "/metadata_plain.xml"), __FILE__." addFileRenamed");
 
         $this->assertTrue($objZip->addFolder(class_resourceloader::getInstance()->getCorePathForModule("module_system")."/module_system/system/config"), __FILE__." addFolder");
 
@@ -31,8 +31,8 @@ class class_test_zip extends class_testbase  {
         $objZip = new class_zip();
         $this->assertTrue($objZip->extractArchive("/files/cache/test.zip", "/files/cache/zipextract"), __FILE__." extractArchive");
 
-        $this->assertFileExists(_realpath_."/files/cache/zipextract".class_resourceloader::getInstance()->getCorePathForModule("module_system")."/module_system/system/licence_lgpl.txt", __FILE__." extractArchive");
-        $this->assertFileExists(_realpath_."/files/cache/zipextract/lgpl_plain.txt", __FILE__." extractArchive");
+        $this->assertFileExists(_realpath_."/files/cache/zipextract".class_resourceloader::getInstance()->getCorePathForModule("module_system")."/module_system/metadata.xml", __FILE__." extractArchive");
+        $this->assertFileExists(_realpath_."/files/cache/zipextract/metadata_plain.xml", __FILE__." extractArchive");
 
         $this->assertFileExists(_realpath_."/files/cache/zipextract".class_resourceloader::getInstance()->getCorePathForModule("module_system")."/module_system/system/config/config.php", __FILE__." extractArchive");
 
@@ -55,9 +55,9 @@ class class_test_zip extends class_testbase  {
         $this->assertFileExists(_realpath_."/files/cache/ziptest", __FILE__." checkFileNotExists");
 
         $objFileSystem->folderCreate("/files/cache/ziptest/subdir");
-        $objFileSystem->fileCopy(class_resourceloader::getInstance()->getCorePathForModule("module_system")."/module_system/system/licence_lgpl.txt", "/files/cache/ziptest/licence_lgpl1.txt");
-        $objFileSystem->fileCopy(class_resourceloader::getInstance()->getCorePathForModule("module_system")."/module_system/system/licence_lgpl.txt", "/files/cache/ziptest/licence_lgpl2.txt");
-        $objFileSystem->fileCopy(class_resourceloader::getInstance()->getCorePathForModule("module_system")."/module_system/system/licence_lgpl.txt", "/files/cache/ziptest/subdir/licence_lgpl.txt");
+        $objFileSystem->fileCopy(class_resourceloader::getInstance()->getCorePathForModule("module_system")."/module_system/metadata.xml", "/files/cache/ziptest/licence_lgpl1.txt");
+        $objFileSystem->fileCopy(class_resourceloader::getInstance()->getCorePathForModule("module_system")."/module_system/metadata.xml", "/files/cache/ziptest/licence_lgpl2.txt");
+        $objFileSystem->fileCopy(class_resourceloader::getInstance()->getCorePathForModule("module_system")."/module_system/metadata.xml", "/files/cache/ziptest/subdir/licence_lgpl.txt");
 
         echo "\ttesting class_zip...\n";
 
@@ -101,14 +101,14 @@ class class_test_zip extends class_testbase  {
         $objZip = new class_zip();
         echo "\topening "._realpath_."/test.zip\n";
         $this->assertTrue($objZip->openArchiveForWriting("/files/cache/test.zip"), __FILE__." openArchive");
-        $this->assertTrue($objZip->addFile(class_resourceloader::getInstance()->getCorePathForModule("module_system")."/module_system/system/licence_lgpl.txt"), __FILE__." addFile");
+        $this->assertTrue($objZip->addFile(class_resourceloader::getInstance()->getCorePathForModule("module_system")."/module_system/metadata.xml"), __FILE__." addFile");
         $this->assertTrue($objZip->closeArchive(), __FILE__." closeArchive");
 
         $this->assertFileExists(_realpath_."/files/cache/test.zip", __FILE__." checkFileExists");
 
         echo "\treading files\n";
-        $strContent = $objZip->getFileFromArchive("/files/cache/test.zip", class_resourceloader::getInstance()->getCorePathForModule("module_system")."/module_system/system/licence_lgpl.txt");
-        $this->assertTrue(uniStrpos($strContent, "Preamble") !== false);
+        $strContent = $objZip->getFileFromArchive("/files/cache/test.zip", class_resourceloader::getInstance()->getCorePathForModule("module_system")."/module_system/metadata.xml");
+        $this->assertTrue(uniStrpos($strContent, "xsi:noNamespaceSchemaLocation=\"http://apidocs.kajona.de/xsd/package.xsd\"") !== false);
 
         echo "\tremoving testfile\n";
         $this->assertTrue($objFileSystem->fileDelete("/files/cache/test.zip"), __FILE__." deleteFile");
