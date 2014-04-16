0cd73f06be23d0e7b263ac7b64bc4b995346bec4
sidler <sidler@mulchprod.de>
2014-04-16 11:11:42 +0200

    NEW: build-scripts -> added a new script cleanCore. used by build_project.xml and buildProject.php in order to remove the excluded modules from the generated zips.

--- _buildfiles/buildProject.php
+++ _buildfiles/buildProject.php
@@ -20,6 +20,8 @@ class BuildHelper {
         echo "   onlySetup: ".$this->bitOnlyProjectsetup."\n";
         echo "\n\n";
 
+
+
         $arrCores = array();
         foreach(scandir(__DIR__."/".$this->strProjectPath) as $strRootFolder) {
             if(strpos($strRootFolder, "core") === false)
@@ -36,20 +38,11 @@ class BuildHelper {
 
         }
 
-        //trigger cleanups if required, e.g. since a module is excluded
-        echo "\n\nSearching for excluded modules at ".__DIR__."/".$this->strProjectPath."/project/system/config/excludedmodules.php"."\n\n";
-        if(file_exists(__DIR__."/".$this->strProjectPath."/project/system/config/excludedmodules.php")) {
-            $arrExcludedModules = array();
-            include(__DIR__."/".$this->strProjectPath."/project/system/config/excludedmodules.php");
-            foreach($arrExcludedModules as $strCore => $arrIgnoredModules) {
-                foreach($arrIgnoredModules as $strOneIgnoredModule) {
-                    if(file_exists(__DIR__."/".$this->strProjectPath."/".$strCore."/".$strOneIgnoredModule)) {
-                        echo " Deleting ".__DIR__."/".$this->strProjectPath."/".$strCore."/".$strOneIgnoredModule."\n";
-                        $this->rrmdir(__DIR__."/".$this->strProjectPath."/".$strCore."/".$strOneIgnoredModule);
-                    }
-                }
-            }
-        }
+        echo "calling cleanCore script: php -f '".__DIR__."/cleanCore.php' '".$this->strProjectPath."'\n";
+        $arrReturn = array();
+        exec("php -f '".__DIR__."/cleanCore.php' '".$this->strProjectPath."'", $arrReturn);
+        echo implode("\n", $arrReturn)."\n";
+
 
         if($this->bitOnlyProjectsetup) {
             return;
@@ -150,22 +143,6 @@ class BuildHelper {
 
     }
 
-    /**
-     * @param $dir
-     * @see http://www.php.net/manual/de/function.rmdir.php#98622
-     */
-    private function rrmdir($dir) {
-        if (is_dir($dir)) {
-            $objects = scandir($dir);
-            foreach ($objects as $object) {
-                if ($object != "." && $object != "..") {
-                    if (filetype($dir."/".$object) == "dir") $this->rrmdir($dir."/".$object); else unlink($dir."/".$object);
-                }
-            }
-            reset($objects);
-            rmdir($dir);
-        }
-    }
 }
 
 $objTestmanager = new BuildHelper();
