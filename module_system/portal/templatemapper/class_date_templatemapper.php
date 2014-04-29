b45150d3b29f6c8824a912e3e54454ad3135f9f5
sidler <sidler@mulchprod.de>
2014-04-29 17:19:57 +0200

    NEW: Added the mapper-interface and two first template-mappers. The default mapper passes the value "as is" and is used as the fallback / default mapper. The date-mapper takes an int/long timestamp and converts it into a readable timestamp.  [ticket #684]

---
+++ module_system/portal/templatemapper/class_date_templatemapper.php
@@ -0,0 +1,25 @@
+<?php
+/*"******************************************************************************************************
+*   (c) 2007-2014 by Kajona, www.kajona.de                                                              *
+*       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
+********************************************************************************************************/
+
+/**
+ * A templatemapper rendering a formatted date
+ *
+ */
+class class_date_templatemapper implements interface_templatemapper {
+
+    /**
+     * Converts the passed value to a formatted value.
+     * In most scenarios, the value is written directly to the template.
+     *
+     * @param mixed $strValue
+     *
+     * @return string
+     */
+    public function format($strValue) {
+        return dateToString(new class_date($strValue));
+    }
+
+} 
---
+++ module_system/portal/templatemapper/class_default_templatemapper.php
@@ -0,0 +1,24 @@
+<?php
+/*"******************************************************************************************************
+*   (c) 2007-2014 by Kajona, www.kajona.de                                                              *
+*       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
+********************************************************************************************************/
+
+/**
+ * A dummy mapper, rendering the value as is back to the template.
+ * This is the default mapper, used of no other mapper is specified.
+ */
+class class_default_templatemapper implements interface_templatemapper {
+    /**
+     * Converts the passed value to a formatted value.
+     * In most scenarios, the value is written directly to the template.
+     *
+     * @param mixed $strValue
+     *
+     * @return string
+     */
+    public function format($strValue) {
+        return $strValue;
+    }
+
+} 
