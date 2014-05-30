54b46e2c8a21a1aa5754c9b4998eac1fa502a7f3
smeyer <stefan.meyer1@yahoo.de>
2014-05-30 16:10:17 +0200

    module_system | properties -> added new properties for objects which can be assigned

--- module_system/lang/module_commons/lang_commons_en.php
+++ module_system/lang/module_commons/lang_commons_en.php
@@ -77,3 +77,5 @@ $lang["commons_validator_int_validationmessage"] = "The field must contain integ
 $lang["commons_validator_numeric_validationmessage"] = "The field must contain numeric values";
 $lang["commons_validator_posint_validationmessage"] = "The field must contain positive integer values";
 $lang["commons_yes"]                     = "Yes";
+
+$lang["commons_remove_assignment"]                     = "Remove assignment";
