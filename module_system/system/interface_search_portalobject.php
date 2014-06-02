75f11a63536571c10d3e522af4ba5f0c3ad52617
sidler <sidler@mulchprod.de>
2014-06-02 15:06:00 +0200

    NEW: module_search | interface_search_portalobject-> changed the semantic of the updateSearchResult method. If necessary, the method may return an array of results instead of only updating the passed result object. This makes sure linked contents (such as media-files or faqs) are found on all sites [ticket #900]

--- module_system/system/interface_search_portalobject.php
+++ module_system/system/interface_search_portalobject.php
@@ -22,11 +22,14 @@ interface interface_search_portalobject extends interface_search_resultobject {
      * This link is rendered by the portal search result generator, so
      * make sure the link is a valid portal page.
      * If you want to suppress the entry from the result, return an empty string instead.
-     *
-     * @see getLinkPortalHref()
+     * If you want to add additional entries to the result set, clone the result and modify
+     * the new instance to your needs. Pack them in an array and they'll be merged
+     * into the result set afterwards.
+     * Make sure to return the passed result-object in this array, too.
      *
      * @param class_search_result $objResult
      *
+     * @see getLinkPortalHref()
      * @return mixed
      */
     public function updateSearchResult(class_search_result $objResult);
