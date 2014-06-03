6c44b6fd32b89ebd82c1892dc66cb41a380bf216
sidler <sidler@mulchprod.de>
2014-06-03 16:15:14 +0200

    BUG: module_system | classloader -> /portal/searchplugins is no longer scanned for class-files

--- module_system/system/class_classloader.php
+++ module_system/system/class_classloader.php
@@ -189,7 +189,6 @@ class class_classloader {
         $this->arrFiles = array_merge($this->arrFiles, $this->getClassesInFolder("/admin/widgets/"));
         $this->arrFiles = array_merge($this->arrFiles, $this->getClassesInFolder("/admin/"));
         $this->arrFiles = array_merge($this->arrFiles, $this->getClassesInFolder("/portal/elements/"));
-        $this->arrFiles = array_merge($this->arrFiles, $this->getClassesInFolder("/portal/searchplugins/"));
         $this->arrFiles = array_merge($this->arrFiles, $this->getClassesInFolder("/portal/templatemapper/"));
         $this->arrFiles = array_merge($this->arrFiles, $this->getClassesInFolder("/portal/"));
         $this->arrFiles = array_merge($this->arrFiles, $this->getClassesInFolder("/system/db/"));
