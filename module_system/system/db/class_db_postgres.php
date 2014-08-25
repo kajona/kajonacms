e2083fb62e25f5dda5aa68de1d04be8bfa10f714
sidler <sidler@mulchprod.de>
2014-08-25 17:15:44 +0200

    NEW: module_system | postgres-driver -> the password is set when dumping / importing the current database

--- module_system/system/db/class_db_postgres.php
+++ module_system/system/db/class_db_postgres.php
@@ -418,18 +418,10 @@ class class_db_postgres extends class_db_base {
      * @return bool
      */
     public function dbExport($strFilename, $arrTables) {
-
         $strFilename = _realpath_.$strFilename;
         $strTables = "-t ".implode(" -t ", $arrTables);
-        $strParamPass = "";
 
-        /*
-        if ($this->strPass != "") {
-        	$strParamPass = " -p".$this->strPass;
-        }
-        */
-
-        $strCommand = $this->strDumpBin." --clean -h".$this->strHost." -U".$this->strUsername.$strParamPass." -p".$this->intPort." ".$strTables." ".$this->strDbName." > \"".$strFilename."\"";
+        $strCommand = "PGPASSWORD=\"".$this->strPass."\" ".$this->strDumpBin." --clean -h".$this->strHost." -U".$this->strUsername." -p".$this->intPort." ".$strTables." ".$this->strDbName." > \"".$strFilename."\"";
         //Now do a systemfork
         $intTemp = "";
         $strResult = system($strCommand, $intTemp);
@@ -445,15 +437,9 @@ class class_db_postgres extends class_db_base {
      * @return bool
      */
     public function dbImport($strFilename) {
-
         $strFilename = _realpath_.$strFilename;
-        $strParamPass = "";
-        /*
-        if ($this->strPass != "") {
-            $strParamPass = " -p".$this->strPass;
-        }
-        */
-        $strCommand = $this->strRestoreBin." -q -h".$this->strHost." -U".$this->strUsername.$strParamPass." -p".$this->intPort." ".$this->strDbName." < \"".$strFilename."\"";
+
+        $strCommand = "PGPASSWORD=\"".$this->strPass."\" ".$this->strRestoreBin." -q -h".$this->strHost." -U".$this->strUsername." -p".$this->intPort." ".$this->strDbName." < \"".$strFilename."\"";
         $intTemp = "";
         $strResult = system($strCommand, $intTemp);
         class_logger::getInstance(class_logger::DBLOG)->addLogRow($this->strRestoreBin." exited with code ".$intTemp, class_logger::$levelInfo);
