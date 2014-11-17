984826895bf729f38c5e9e0346d80b24bd6eba2c
sidler <sidler@mulchprod.de>
2014-11-17 10:49:14 +0100

    NEW: module_dashoard | widget weather -> changed the remote API from Yahoo to openweathermap

--- module_dashboard/admin/widgets/class_adminwidget_weather.php
+++ module_dashboard/admin/widgets/class_adminwidget_weather.php
@@ -38,7 +38,6 @@ class class_adminwidget_weather extends class_adminwidget implements interface_a
             $this->getLang("weather_unit"),
             $this->getFieldValue("unit")
         );
-        $strReturn .= $this->objToolkit->formTextRow($this->getLang("weather_location_finder"));
         $strReturn .= $this->objToolkit->formInputText("location", $this->getLang("weather_location"), $this->getFieldValue("location"));
         return $strReturn;
     }
@@ -53,52 +52,45 @@ class class_adminwidget_weather extends class_adminwidget implements interface_a
     public function getWidgetOutput() {
         $strReturn = "";
 
+        if($this->getFieldValue("location") == "") {
+            return "Please set up a location";
+        }
+
+        if(uniStrpos($this->getFieldValue("location"), "GM") !== false) {
+            return "This widget changed, please update your location by editing the widget";
+        }
+
+
         //request the xml...
         try {
+
+            $strFormat = "metric";
+            if($this->getFieldValue("unit") == "f")
+                $strFormat = "imperial";
+
             $objRemoteloader = new class_remoteloader();
-            $objRemoteloader->setStrHost("weather.yahooapis.com");
-            $objRemoteloader->setStrQueryParams("/forecastrss?p=" . $this->getFieldValue("location") . "&u=" . $this->getFieldValue("unit"));
+            $objRemoteloader->setStrHost("api.openweathermap.org");
+            $objRemoteloader->setStrQueryParams("/data/2.5/forecast/daily?q=" . $this->getFieldValue("location") . "&units=" . $strFormat."&cnt=4");
             $strContent = $objRemoteloader->getRemoteContent();
         }
         catch(class_exception $objExeption) {
             $strContent = "";
         }
 
-        if($strContent != "") {
-            $objXmlparser = new class_xml_parser();
-            $objXmlparser->loadString($strContent);
+        if($strContent != "" && json_decode($strContent, true) !== null) {
 
-            $arrWeather = $objXmlparser->xmlToArray();
-            if(isset($arrWeather["rss"])) {
-                if(isset($arrWeather["rss"][0]["channel"][0]["yweather:location"][0]["attributes"]["city"])) {
-                    $strReturn .= $this->widgetText($this->getLang("weather_location_string") . $arrWeather["rss"][0]["channel"][0]["yweather:location"][0]["attributes"]["city"]);
-                }
+            $arrResponse = json_decode($strContent, true);
 
-                if(isset($arrWeather["rss"][0]["channel"][0]["item"][0]["yweather:condition"][0]["attributes"]["temp"])) {
-                    $strReturn .= $this->widgetText(
-                        $this->getLang("weather_temp_string")
-                        . $arrWeather["rss"][0]["channel"][0]["item"][0]["yweather:condition"][0]["attributes"]["temp"] . " °"
-                        . $arrWeather["rss"][0]["channel"][0]["yweather:units"][0]["attributes"]["temperature"]
-                    );
-                }
 
-                $strReturn .= $this->widgetSeparator();
-                $strReturn .= $this->widgetText($this->getLang("weather_forecast"));
+            $strReturn .= $this->widgetText($this->getLang("weather_location_string") . $arrResponse["city"]["name"].", ".$arrResponse["city"]["country"]);
 
-                $strReturn .= $this->widgetText(
-                    $arrWeather["rss"][0]["channel"][0]["item"][0]["yweather:forecast"][0]["attributes"]["date"] . ": "
-                    . $arrWeather["rss"][0]["channel"][0]["item"][0]["yweather:forecast"][0]["attributes"]["high"]
-                    . " °" . $arrWeather["rss"][0]["channel"][0]["yweather:units"][0]["attributes"]["temperature"]
-                );
 
-                $strReturn .= $this->widgetText(
-                    $arrWeather["rss"][0]["channel"][0]["item"][0]["yweather:forecast"][1]["attributes"]["date"] . ": "
-                    . $arrWeather["rss"][0]["channel"][0]["item"][0]["yweather:forecast"][1]["attributes"]["high"]
-                    . " °" . $arrWeather["rss"][0]["channel"][0]["yweather:units"][0]["attributes"]["temperature"]
-                );
-            }
-            else {
-                $strReturn .= $this->getLang("weather_errorloading");
+            foreach($arrResponse["list"] as $arrOneForecast) {
+                $objDate = new class_date($arrOneForecast["dt"]);
+                $strReturn .= "<div>";
+                $strReturn .= $this->widgetText("<div style='float: left;'>".dateToString($objDate, false).": ".round($arrOneForecast["temp"]["day"], 1)."°</div>");
+                $strReturn .= $this->widgetText("<img src='//openweathermap.org/img/w/".$arrOneForecast["weather"][0]["icon"].".png' style='float: right;' />");
+                $strReturn .= "</div><div style='clear: both;'></div>";
             }
 
         }
