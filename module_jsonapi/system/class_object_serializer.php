0e3a29e3ba607e309adde9e182809e98a62071a3
Christoph Kappestein <k42b3.x@gmail.com>
2015-02-05 20:09:56 +0100

    removed mapper system and cleanup of unused methods

--- module_jsonapi/system/class_object_serializer.php
+++ module_jsonapi/system/class_object_serializer.php
@@ -9,8 +9,7 @@
 /**
  * The class_object_serializer is based on the class_template_mapper. Returns 
  * all properties of an class which has an @jsonExport annotation. These 
- * properties are exposed through the json api. Currently the @jsonMapper 
- * annotation uses the same mapper classes as the @templateMapper
+ * properties are exposed through the json api
  *
  * @package module_system
  * @author sidler@mulchprod.de
@@ -22,7 +21,6 @@
 class class_object_serializer {
 
     const STR_ANNOTATION_JSONEXPORT = "@jsonExport";
-    const STR_ANNOTATION_JSONMAPPER = "@jsonMapper";
 
     /** @var class_root */
     private $objObject = null;
@@ -53,26 +51,6 @@ class class_object_serializer {
     }
 
     /**
-     * Adds a single entry to the current set of mapped values
-     *
-     * @param string $strName
-     * @param string $strValue
-     * @param string $strTemplateMapper
-     *
-     * @return void
-     */
-    public function addPlaceholder($strName, $strValue, $strTemplateMapper = "default") {
-        try {
-            $objMapper = $this->getMapperInstance($strTemplateMapper);
-            $strValue = $objMapper->format($strValue);
-        }
-        catch(class_exception $objException) {
-            $strValue = $objException->getMessage();
-        }
-        $this->arrMapping[$strName] = $strValue;
-    }
-
-    /**
      * Reads the properties marked with templateExport from the current object
      *
      * @return void
@@ -84,29 +62,13 @@ class class_object_serializer {
         foreach($properties as $strOneProperty) {
             $strGetter = $objReflection->getGetter($strOneProperty);
 
-            //get the jsonmapper
-            $strMapper = $objReflection->getAnnotationValueForProperty($strOneProperty, self::STR_ANNOTATION_JSONMAPPER);
-            if($strMapper == null)
-                $strMapper = "default";
+            $strValue = call_user_func(array($this->objObject, $strGetter));
+            if ($strValue instanceof class_date) {
+                $strValue = date(DateTime::ATOM, $strValue->getTimeInOldStyle());
+            }
 
-            $this->addPlaceholder($strOneProperty, call_user_func(array($this->objObject, $strGetter)), $strMapper);
+            $this->arrMapping[$strOneProperty] = $strValue;
         }
-    }
-
-    /**
-     * Loads the validator identified by the passed name.
-     *
-     * @param string $strName
-     * @return interface_templatemapper
-     * @throws class_exception
-     */
-    private function getMapperInstance($strName) {
-        $strClassname = "class_".$strName."_templatemapper";
-        if(class_resourceloader::getInstance()->getPathForFile("/portal/templatemapper/".$strClassname.".php")) {
-            return new $strClassname();
-        }
-        else
-            throw new class_exception("failed to load validator of type ".$strClassname, class_exception::$level_ERROR);
     }
 
     /**
