c8cc38f662635081fd1010a31ee75a551af27ecf
sidler <sidler@mulchprod.de>
2015-02-17 09:35:29 +0100

    bumped copyright to 2015

--- module_system/system/class_systeminfo_webserver.php
+++ module_system/system/class_systeminfo_webserver.php
@@ -1,6 +1,6 @@
 <?php
 /*"******************************************************************************************************
-*   (c) 2007-2014 by Kajona, www.kajona.de                                                              *
+*   (c) 2007-2015 by Kajona, www.kajona.de                                                              *
 *       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
 *-------------------------------------------------------------------------------------------------------*
 *	$Id: class_changelog_provider_settings.php 6322 2014-01-02 08:31:49Z sidler $                      *
--- module_v4skin/admin/skins/kajona_v4/js/v4skin.js
+++ module_v4skin/admin/skins/kajona_v4/js/v4skin.js
@@ -1,4 +1,4 @@
-//   (c) 2007-2014 by Kajona, www.kajona.de
+//   (c) 2007-2015 by Kajona, www.kajona.de
 //       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt
 //       $Id$
 
