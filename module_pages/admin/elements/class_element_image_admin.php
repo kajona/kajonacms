3de5e7acb61909bc6f0148754b56cd0cb489df97
sidler <sidler@mulchprod.de>
2015-02-24 09:45:22 +0100

    BUG: module_pages | image element -> saving the element could fail in some combinations with special mysql versions

--- module_pages/admin/elements/class_element_image_admin.php
+++ module_pages/admin/elements/class_element_image_admin.php
@@ -62,7 +62,7 @@ class class_element_image_admin extends class_element_admin implements interface
      * @fieldLabel image_x
      * @fieldHidden
      */
-    private $strImageX = "";
+    private $strImageX = 0;
 
     /**
      * @var string
@@ -73,7 +73,7 @@ class class_element_image_admin extends class_element_admin implements interface
      * @fieldLabel image_y
      * @fieldHidden
      */
-    private $strImageY = "";
+    private $strImageY = 0;
 
     /**
      * @var string
