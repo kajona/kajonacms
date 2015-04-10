532d7314b53f82fd94cde20860997dbbce3f12c2
Christoph Kappestein <christoph.kappestein@artemeon.de>
2015-04-10 16:05:34 +0200

    FIX: module_system | testbase_object > improve test base so that it is module independent

--- module_system/system/class_testbase_object.php
+++ module_system/system/class_testbase_object.php
@@ -3,17 +3,13 @@
 require_once __DIR__."/class_testbase.php";
 
 /**
- * The class_testbase is the common baseclass for all testcases which need to test against an object tree
+ * Base class to create object structures which can be used to test against an object tree
  *
  * @package module_system
  * @since 3.4
  * @author sidler@mulchprod.de
  */
 abstract class class_testbase_object extends class_testbase {
-
-    protected $arrObjects = array();
-    protected $arrOes = array();
-    protected $arrDimensions = array();
 
     protected function setUp() {
 
@@ -27,38 +23,10 @@ abstract class class_testbase_object extends class_testbase {
         $objDom = new DOMDocument();
         $objDom->load($strFile);
 
-        $objObject = null;
-        $objOe = null;
-        $objDimension = null;
-        foreach($objDom->documentElement->childNodes as $objChild) {
-            if($objChild instanceof DOMElement) {
-                if($objChild->nodeName == 'object') {
-                    $objObject = $objChild;
-                }
-                else if($objChild->nodeName == 'oe') {
-                    $objOe = $objChild;
-                }
-                else if($objChild->nodeName == 'dimension') {
-                    $objDimension = $objChild;
-                }
-            }
-        }
-
-        // we must create first oes and dimensions so that objects can refer to them
-        if(!empty($objDimension)) {
-            $this->createDimensionStructure($objDimension);
-        }
-
-        if(!empty($objOe)) {
-            $this->createOeStructure($objOe);
-        }
-
-        if(!empty($objObject)) {
-            $this->createObjectStructure($objObject);
-        }
+        $this->createStructure($objDom->documentElement);
 
         $this->flushDBCache();
-        class_module_prozessverwaltung_dimension::resetCache();
+
     }
 
     protected function tearDown() {
@@ -69,30 +37,86 @@ abstract class class_testbase_object extends class_testbase {
 
     }
 
-    protected function createObjectStructure(DOMElement $objElement, $objParent = null)
+    /**
+     * Returns an path to an xml fixture file which can be used to create and delete database structures
+     *
+     * @return string
+     */
+    abstract protected function getFixtureFile();
+
+    /**
+     * Creates the database structure like defined in the xml element
+     *
+     * @param DOMElement $objElement
+     * @return void
+     */
+    abstract protected function createStructure(DOMElement $objElement);
+
+    /**
+     * Cleans up the database structure
+     *
+     * @return void
+     */
+    abstract protected function cleanStructure();
+
+    /**
+     * Returns the default root id for an given class name
+     *
+     * @param string $strClassName
+     * @return string
+     */
+    abstract protected function getDefaultRootId($strClassName);
+
+    /**
+     * Is called if an object gets created can be used to store the reference which can be used later in the test
+     *
+     * @param string $strName
+     * @param string $objObject
+     */
+    abstract protected function addObject($strName, class_model $objObject);
+
+    /**
+     * Returns an object for an given reference name
+     *
+     * @param $strName
+     * @return class_model
+     */
+    abstract protected function getObject($strName);
+
+    /**
+     * Assigns an reference to an object
+     *
+     * @param $objSource
+     * @param $objReference
+     * @return mixed
+     */
+    abstract protected function assignReferenceToObject(class_model $objSource, class_model $objReference);
+
+    protected function createDataStructure(DOMElement $objElement, $objParent = null)
     {
         $arrParameters = array();
         foreach($objElement->attributes as $objAttr) {
-            if(!in_array($objAttr->nodeName, array('class', 'name', 'assignments'))) {
-                $arrParameters[$objAttr->nodeName] = (string) $objAttr->nodeValue;
+            if(!in_array($objAttr->nodeName, array('class', 'name'))) {
+                $arrParameters[$objAttr->nodeName] = $objAttr->nodeValue;
             }
         }
 
-        $strParentId = $objParent === null ? class_module_prozessverwaltung_prozess::getProcessRootId() : $objParent->getStrSystemid();
         $strName = $objElement->getAttribute('name');
-        $arrAssignments = explode(',', $objElement->getAttribute('assignments'));
 
         $strClassName = $objElement->getAttribute('class');
         if(empty($strClassName)) {
-            throw new RuntimeException('No class name given for object "' . $strName . '" (' . $objElement->getNodePath() . ')');
+            throw new RuntimeException('No class name given for oe "' . $strName . '" (' . $objElement->getNodePath() . ')');
         }
+
+        $strParentId = $objParent === null ? $this->getDefaultRootId($strClassName) : $objParent->getStrSystemid();
 
         // resolve references
         foreach($arrParameters as $strKey => $strValue) {
             if(substr($strValue, 0, 4) == 'ref:') {
                 $strRef = trim(substr($strValue, 4));
-                if(isset($this->arrObjects[$strRef])) {
-                    $arrParameters[$strKey] = $this->arrObjects[$strRef]->getStrSystemid();
+                $objRef = $this->getObject($strRef);
+                if($objRef instanceof class_model) {
+                    $arrParameters[$strKey] = $objRef->getStrSystemid();
                 }
                 else {
                     throw new RuntimeException('Object "' . $strName . '" refers to an non existing object (' . $objElement->getNodePath() . ')');
@@ -102,126 +126,28 @@ abstract class class_testbase_object extends class_testbase {
 
         $objObject = $this->createObject($strClassName, $strParentId, array(), $arrParameters, false);
 
-        if(isset($this->arrObjects[$strName])) {
-            throw new RuntimeException('Object name "' . $strName . '" already exists (' . $objElement->getNodePath() . ')');
-        }
+        $this->addObject($strName, $objObject);
 
-        $this->arrObjects[$strName] = $objObject;
-
-        foreach($objElement->childNodes as $objNode) {
-            if($objNode instanceof DOMElement) {
-                $this->createObjectStructure($objNode, $objObject);
-            }
-        }
+        // handle assignments
+        $arrAssignments = explode(',', $objElement->getAttribute('assignments'));
 
         foreach($arrAssignments as $strName) {
             $strName = trim($strName);
             if(!empty($strName)) {
-                if(isset($this->arrOes[$strName])) {
-                    $objObject->setStrOeId($this->arrOes[$strName]->getStrSystemid());
-                }
-                else if(isset($this->arrDimensions[$strName])) {
-                    $objObject->addAssignedUnterdimension($this->arrDimensions[$strName]->getStrSystemid());
-                }
-                else {
-                    throw new RuntimeException('Object "' . $objObject->getStrSystemid() . '" refers to an non existing object "' . $strName . '" (' . $objElement->getNodePath() . ')');
+                $objReference = $this->getObject($strName);
+                if($objReference instanceof class_model) {
+                    $this->assignReferenceToObject($objObject, $objReference);
                 }
             }
         }
 
         $objObject->updateObjectToDb();
-    }
 
-    protected function createOeStructure(DOMElement $objElement, $objParent = null)
-    {
-        $arrParameters = array();
-        foreach($objElement->attributes as $objAttr) {
-            if(!in_array($objAttr->nodeName, array('class', 'name'))) {
-                $arrParameters[$objAttr->nodeName] = $objAttr->nodeValue;
-            }
-        }
-
-        $strParentId = $objParent === null ? class_module_prozessverwaltung_oe::getOeRootId() : $objParent->getStrSystemid();
-        $strName = $objElement->getAttribute('name');
-
-        $strClassName = $objElement->getAttribute('class');
-        if(empty($strClassName)) {
-            throw new RuntimeException('No class name given for oe "' . $strName . '" (' . $objElement->getNodePath() . ')');
-        }
-
-        $objObject = $this->createObject($strClassName, $strParentId, array(), $arrParameters, false);
-
-        if(isset($this->arrOes[$strName])) {
-            throw new RuntimeException('Oe name "' . $strName . '" already exists (' . $objElement->getNodePath() . ')');
-        }
-
-        $this->arrOes[$strName] = $objObject;
-
+        // walk through child elements
         foreach($objElement->childNodes as $objNode) {
             if($objNode instanceof DOMElement) {
-                $this->createOeStructure($objNode, $objObject);
+                $this->createDataStructure($objNode, $objObject);
             }
         }
     }
-
-    protected function createDimensionStructure(DOMElement $objElement, $objParent = null)
-    {
-        $arrParameters = array();
-        foreach($objElement->attributes as $objAttr) {
-            if(!in_array($objAttr->nodeName, array('class', 'name'))) {
-                $arrParameters[$objAttr->nodeName] = $objAttr->nodeValue;
-            }
-        }
-
-        $strParentId = $objParent === null ? class_module_prozessverwaltung_dimension::getDimensionRootId() : $objParent->getStrSystemid();
-        $strName = $objElement->getAttribute('name');
-
-        $strClassName = $objElement->getAttribute('class');
-        if(empty($strClassName)) {
-            throw new RuntimeException('No class name given for dimension "' . $strName . '" (' . $objElement->getNodePath() . ')');
-        }
-
-        $objObject = $this->createObject($strClassName, $strParentId, array(), $arrParameters, false);
-
-        if(isset($this->arrDimensions[$strName])) {
-            throw new RuntimeException('Dimension name "' . $strName . '" already exists (' . $objElement->getNodePath() . ')');
-        }
-
-        $this->arrDimensions[$strName] = $objObject;
-
-        foreach($objElement->childNodes as $objNode) {
-            if($objNode instanceof DOMElement) {
-                $this->createDimensionStructure($objNode, $objObject);
-            }
-        }
-    }
-
-    protected function cleanStructure()
-    {
-        $arrDimensions = array_reverse($this->arrDimensions);
-        foreach($arrDimensions as $objObject) {
-            /** @var $objObject class_module_prozessverwaltung_dimension */
-            $objObject->deleteObject();
-        }
-
-        $arrOes = array_reverse($this->arrOes);
-        foreach($arrOes as $objObject) {
-            /** @var $objObject class_module_prozessverwaltung_oe */
-            $objObject->deleteObjectFromDatabase();
-        }
-
-        $arrObjects = array_reverse($this->arrObjects);
-        foreach($arrObjects as $objObject) {
-            /** @var $objObject class_module_prozessverwaltung_prozess */
-            $objObject->deleteObjectFromDatabase();
-        }
-    }
-
-    /**
-     * Returns an path to an xml fixture file which can be used to create and delete object database structures for
-     * testing
-     *
-     * @return string
-     */
-    abstract protected function getFixtureFile();
 }
