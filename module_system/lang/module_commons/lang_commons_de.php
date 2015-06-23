45fd0eb9cc576e45430e9ee10f036762016b9e64
smeyer <stefan.meyer@artemeon.de>
2015-06-23 11:07:23 +0200

    NEW: module_system | properties -> added new property for last modification date

--- module_system/lang/module_commons/lang_commons_de.php
+++ module_system/lang/module_commons/lang_commons_de.php
@@ -32,6 +32,7 @@ $lang["commons_create_folder"]           = "Neuer Ordner";
 $lang["commons_continue"]                = "Weiter";
 $lang["commons_close"]                   = "Schließen";
 $lang["commons_date"]                    = "Datum";
+$lang["commons_lastmodification_date"]   = "Letztes Änderungsdatum";
 $lang["commons_delete"]                  = "Löschen";
 $lang["commons_delete_category_question"] = "Möchten Sie die Kategorie &quot;<b>%%element_name%%</b>&quot; wirklich löschen?";
 $lang["commons_delete_error"]            = "Fehler beim Löschen des Datensatzes";
