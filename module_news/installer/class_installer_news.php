c5d174058a2ee2ad5a29ca2813592c2c85ec00d8
sidler <sidler@mulchprod.de>
2015-07-15 09:44:25 +0200

    BUG: module_news | admin_element -> the view-mode (archive vs. current news) was hidden due to a naming conflict (thx to mintdata)

--- module_news/installer/class_installer_news.php
+++ module_news/installer/class_installer_news.php
@@ -242,6 +242,13 @@ class class_installer_news extends class_installer_base implements interface_ins
             $this->updateElementVersion("news", "4.7.1");
         }
 
+        $arrModule = class_module_system_module::getPlainModuleData($this->objMetadata->getStrTitle(), false);
+        if($arrModule["module_version"] == "4.7.1") {
+            $strReturn .= "Updating to 4.7.2...\n";
+            $this->updateModuleVersion("news", "4.7.2");
+            $this->updateElementVersion("news", "4.7.2");
+        }
+
         return $strReturn."\n\n";
 	}
 
