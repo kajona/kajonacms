581d11849975fcd38276fca746676006a5f83e7e
Christoph Kappestein <christoph.kappestein@artemeon.de>
2015-10-12 16:11:34 +0200

    NEW: module_workflows | class_module_workflow_todo_provider_base > new base todo provider which simplifies creating a todo provider based on specific workflow classes

---
+++ module_workflows/system/class_module_workflow_todo_provider_base.php
@@ -0,0 +1,63 @@
+<?php
+/*"******************************************************************************************************
+*   (c) 2015 by Kajona, www.kajona.de                                                              *
+*       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
+********************************************************************************************************/
+
+/**
+ * @package module_workflows
+ */
+abstract class class_module_workflow_todo_provider_base implements interface_todo_provider
+{
+    public static function getExtensionName()
+    {
+        return self::EXTENSION_POINT;
+    }
+
+    public function getCurrentTodosByCategory($strCategory)
+    {
+        if (in_array($strCategory, array_keys($this->getWorkflowClasses()))) {
+            return $this->getPendingWorkflows($strCategory);
+        } else {
+            return array();
+        }
+    }
+
+    public function getCategories()
+    {
+        return $this->getWorkflowClasses();
+    }
+
+    protected function getPendingWorkflows($strWorkflowClass)
+    {
+        $objLang = class_lang::getInstance();
+        $arrUsers = array_merge(array(class_session::getInstance()->getUserID()), class_session::getInstance()->getGroupIdsAsArray());
+        $arrWorkflows = class_module_workflows_workflow::getPendingWorkflowsForUser($arrUsers, false, false, array($strWorkflowClass));
+        $arrResult = array();
+
+        foreach ($arrWorkflows as $objWorkflow) {
+            if ($objWorkflow->getObjWorkflowHandler()->providesUserInterface()) {
+                /** @var class_module_workflows_workflow $objWorkflow */
+                $objTodo = new class_todo_entry();
+                $objTodo->setStrIcon($objWorkflow->getStrIcon());
+                $objTodo->setStrCategory($strWorkflowClass);
+                $objTodo->setStrDisplayName($objWorkflow->getStrDisplayName());
+                $objTodo->setArrModuleNavi(array(
+                    class_link::getLinkAdmin("workflows", "showUI", "&systemid=" . $objWorkflow->getSystemid(), "", $objLang->getLang("workflow_ui", "workflows"), "icon_workflow_ui")
+                ));
+
+                $arrResult[] = $objTodo;
+            }
+        }
+
+        return $arrResult;
+    }
+
+    /**
+     * Returns an array containing all classes
+     *
+     * @return array<workflow_class => "category label">
+     */
+    abstract protected function getWorkflowClasses();
+}
+
