2f5e3d9396557c9586ff40cad425999714f46d91
Christoph Kappestein <christoph.kappestein@artemeon.de>
2015-11-17 18:00:56 +0100

    NEW: add jasmine js test framework

---
+++ module_dashboard/admin/scripts/spec/DashboardSpec.js
@@ -0,0 +1,15 @@
+
+include('../../core/module_system/system/scripts/loader.js');
+include('../../core/module_system/admin/scripts/kajona.js');
+include('../../core/module_dashboard/admin/scripts/dashboard.js');
+
+describe("dashboard.js", function() {
+
+    beforeEach(function() {
+    });
+
+    it("test nothing", function() {
+        expect(0).toBe(0);
+    });
+
+});
---
+++ module_system/admin/scripts/spec/KajonaSpec.js
@@ -0,0 +1,19 @@
+
+include('../../core/module_system/system/scripts/loader.js');
+include('../../core/module_system/admin/scripts/kajona.js');
+
+describe("kajona.js", function() {
+
+    beforeEach(function() {
+    });
+
+    it("test is touch device", function() {
+        expect(KAJONA.util.isTouchDevice()).toBe(0);
+    });
+
+    it("test in array", function() {
+        expect(KAJONA.util.inArray("foo", ["bar", "foo"])).toBe(true);
+        expect(KAJONA.util.inArray("baz", ["bar", "foo"])).toBe(false);
+    });
+
+});
