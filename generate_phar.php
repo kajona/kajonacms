5a0e2b2d68dda63471dd992b37d5818ef95bcd1c
sidler <sidler@mulchprod.de>
2016-01-11 17:54:53 +0100

    UPDATE: module_system | generate_phar -> the script is capable of various core locations

--- generate_phar.php
+++ generate_phar.php
@@ -1,53 +1,66 @@
 <?php
 
-$arrFiles = scandir(".");
 
-foreach ($arrFiles as $strFile) {
+$strDeployPath = "/Users/sidler/web/agp_v5_master_phar";
 
-    if (is_dir($strFile) && substr($strFile, 0, 7) == 'module_') {
 
-        $moduleName = substr($strFile, 7);
-        $pharName = "module_".$moduleName.".phar";
+$arrCores = scandir("./../");
 
-        $strTargetPath = __DIR__."/".$pharName;
-        if (is_dir("/Users/sidler/web/kajona_phar_only/core")) {
-            $strTargetPath = "/Users/sidler/web/kajona_phar_only/core/".$pharName;
-        }
+foreach($arrCores as $strOneCore) {
 
-        $phar = new Phar(
-            $strTargetPath,
-            FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME,
-            $pharName
-        );
-        $phar->buildFromDirectory(__DIR__."/module_".$moduleName);
-        $phar->setStub($phar->createDefaultStub());
-        // Compression with ZIP or GZ?
-        //$phar->convertToExecutable(Phar::ZIP);
-        //$phar->compress(Phar::GZ);
-        echo 'Generated phar '.$pharName."\n";
+    if(strpos($strOneCore, "core") === false) {
+        continue;
     }
 
-    if(is_dir($strFile) && substr($strFile, 0, 8) == 'element_')   {
+    $arrFiles = scandir("./../".$strOneCore);
 
-        $moduleName = substr($strFile, 8);
-        $pharName = "element_".$moduleName.".phar";
+    foreach ($arrFiles as $strFile) {
 
-        $strTargetPath = __DIR__."/".$pharName;
-        if (is_dir("/Users/sidler/web/kajona_phar_only/core")) {
-            $strTargetPath = "/Users/sidler/web/kajona_phar_only/core/".$pharName;
+        if (is_dir(__DIR__."/../".$strOneCore."/".$strFile) && substr($strFile, 0, 7) == 'module_') {
+
+            $strModuleName = substr($strFile, 7);
+            $strPharName = "module_".$strModuleName.".phar";
+
+            $strTargetPath = __DIR__."/../".$strOneCore."/".$strPharName;
+            if ($strDeployPath != "" && is_dir($strDeployPath."/".$strOneCore)) {
+                $strTargetPath = $strDeployPath."/".$strOneCore."/".$strPharName;
+            }
+
+            $phar = new Phar(
+                $strTargetPath,
+                FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME,
+                $strPharName
+            );
+            $phar->buildFromDirectory(__DIR__."/../".$strOneCore."/module_".$strModuleName);
+            $phar->setStub($phar->createDefaultStub());
+            // Compression with ZIP or GZ?
+            //$phar->convertToExecutable(Phar::ZIP);
+            //$phar->compress(Phar::GZ);
+            echo 'Generated phar '.$strPharName."\n";
         }
 
-        $phar = new Phar(
-            $strTargetPath,
-            FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME,
-            $pharName
-        );
-        $phar->buildFromDirectory(__DIR__."/element_".$moduleName);
-        $phar->setStub($phar->createDefaultStub());
-        // Compression with ZIP or GZ?
-        //$phar->convertToExecutable(Phar::ZIP);
-        //$phar->compress(Phar::GZ);
-        echo 'Generated phar '.$pharName."\n";
-    }
+        if (is_dir(__DIR__."/../".$strOneCore."/".$strFile) && substr($strFile, 0, 8) == 'element_') {
 
+            $strModuleName = substr($strFile, 8);
+            $strPharName = "element_".$strModuleName.".phar";
+
+            $strTargetPath = __DIR__."/../".$strOneCore."/".$strPharName;
+            if ($strDeployPath != "" && is_dir($strDeployPath."/".$strOneCore)) {
+                $strTargetPath = $strDeployPath."/".$strOneCore."/".$strPharName;
+            }
+
+            $phar = new Phar(
+                $strTargetPath,
+                FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME,
+                $strPharName
+            );
+            $phar->buildFromDirectory(__DIR__."/../".$strOneCore."/element_".$strModuleName);
+            $phar->setStub($phar->createDefaultStub());
+            // Compression with ZIP or GZ?
+            //$phar->convertToExecutable(Phar::ZIP);
+            //$phar->compress(Phar::GZ);
+            echo 'Generated phar '.$strPharName."\n";
+        }
+
+    }
 }
