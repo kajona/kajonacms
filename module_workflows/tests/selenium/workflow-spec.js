146792100017ebdc2b8b7e7887d05c0eb6417d11
Christoph Kappestein <christoph.kappestein@artemeon.de>
2016-01-13 15:56:14 +0100

    UPDATE: module_workflows | workflow-spec> fix test

--- module_workflows/tests/selenium/workflow-spec.js
+++ module_workflows/tests/selenium/workflow-spec.js
@@ -8,12 +8,7 @@ describe('module_workflows', function() {
     it('test list', function() {
         browser.get('http://127.0.0.1:8080/index.php?admin=1&module=workflows&action=list');
 
-        var workflows = element.all(by.css('.title'));
-
-        expect(workflows.count()).toEqual(3);
-        expect(workflows.get(0).getText()).toEqual('Zusammenfassung neuer Nachrichten');
-        expect(workflows.get(1).getText()).toEqual('Berechnung Enddatum Objekte');
-        expect(workflows.get(2).getText()).toEqual('Maßnahme');
+        expect(browser.driver.findElement(by.id('moduleTitle')).getText()).toEqual('Workflows');
     });
 
 });
