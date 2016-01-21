613e6fdf85c0573ba1cab9a43124d9cf351e23ed
Christoph Kappestein <christoph.kappestein@artemeon.de>
2016-01-21 16:55:09 +0100

    UPDATE: _buildfiles | setupSeleniumConfig.php > read bootstrap also from phar

--- _buildfiles/setupSeleniumConfig.php
+++ _buildfiles/setupSeleniumConfig.php
@@ -1,7 +1,10 @@
 <?php
 
-require_once './core/module_system/bootstrap.php';
-
+if (is_dir(__DIR__."/core/module_system/")) {
+    require_once __DIR__.'/core/module_system/bootstrap.php';
+} else {
+    require_once 'phar://'.__DIR__.'/core/module_system.phar/bootstrap.php';
+}
 
 $objDb = class_carrier::getInstance()->getObjDB();
 
