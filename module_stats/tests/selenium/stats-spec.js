deb101881a8ef1ec5256db1044d446c396acb821
Christoph Kappestein <christoph.kappestein@artemeon.de>
2016-01-22 15:19:09 +0100

    UPDATE: module_stats | stats-spec.js > debug error

--- module_stats/tests/selenium/stats-spec.js
+++ module_stats/tests/selenium/stats-spec.js
@@ -1,4 +1,6 @@
 
+var fs = require('fs');
+
 describe('module_stats', function() {
 
     beforeEach(function() {
@@ -7,6 +9,12 @@ describe('module_stats', function() {
 
     it('test list', function() {
         browser.get('http://127.0.0.1:8080/index.php?admin=1&module=stats&action=list');
+
+        browser.takeScreenshot().then(function(png) {
+            var stream = fs.createWriteStream('debug.png');
+            stream.write(new Buffer(png, 'base64'));
+            stream.end();
+        });
 
         expect(browser.driver.findElement(by.id('moduleTitle')).getText()).toEqual('Statistiken');
     });
