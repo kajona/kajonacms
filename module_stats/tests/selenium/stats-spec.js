0f1940ea956bc7f0fe011cda815d5241bd898c61
Christoph Kappestein <christoph.kappestein@artemeon.de>
2016-01-22 15:43:11 +0100

    UPDATE: module_stats | stats-spec.js > remove debug

--- module_stats/tests/selenium/stats-spec.js
+++ module_stats/tests/selenium/stats-spec.js
@@ -1,6 +1,4 @@
 
-var fs = require('fs');
-
 describe('module_stats', function() {
 
     beforeEach(function() {
@@ -9,12 +7,6 @@ describe('module_stats', function() {
 
     it('test list', function() {
         browser.get('http://127.0.0.1:8080/index.php?admin=1&module=stats&action=list');
-
-        browser.takeScreenshot().then(function(png) {
-            var stream = fs.createWriteStream('debug.png');
-            stream.write(new Buffer(png, 'base64'));
-            stream.end();
-        });
 
         expect(browser.driver.findElement(by.id('moduleTitle')).getText()).toEqual('Statistiken');
     });
