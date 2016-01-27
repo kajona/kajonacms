3b9cd5d614439cb44897aced8ca369df314b8e15
sidler <sidler@mulchprod.de>
2016-01-27 14:39:20 +0100

    NEW: module_system | lang.js -> moved the ajax lang-property loader to KAJONA.util NEW: module_system | lang.js -> the cache-manager creates an entry per module

--- module_system/system/class_lang.php
+++ module_system/system/class_lang.php
@@ -153,6 +153,8 @@ class class_lang {
      * Returns all properties for a specific module
      *
      * @param string $strModule
+     * @todo merge with commons entries
+     * @todo switch to fallback language
      */
     public function getProperties($strModule)
     {
