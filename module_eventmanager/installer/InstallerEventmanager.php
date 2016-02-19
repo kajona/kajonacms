2b3782c4d6938234d876295423725d60522e5693
sidler <sidler@mulchprod.de>
2016-02-19 10:46:24 +0100

    BUG: module_eventmanager | InstallerEventmanager.php -> added a namespace

--- module_eventmanager/installer/InstallerEventmanager.php
+++ module_eventmanager/installer/InstallerEventmanager.php
@@ -6,6 +6,9 @@
 *-------------------------------------------------------------------------------------------------------*
 *	$Id$                                       *
 ********************************************************************************************************/
+
+namespace Kajona\Eventmanager\Installer;
+
 use Kajona\Eventmanager\System\EventmanagerEvent;
 use Kajona\Pages\System\PagesElement;
 use Kajona\System\System\Carrier;
