ff0f71a99e352c28b6a02099f7bf4598087ae416
Christoph Kappestein <christoph.kappestein@artemeon.de>
2016-03-08 16:19:58 +0100

    UPDATE: _buildfiles | setupSeleniumConfig.php > set user lang

--- _buildfiles/setupSeleniumConfig.php
+++ _buildfiles/setupSeleniumConfig.php
@@ -8,6 +8,6 @@ if (is_dir(__DIR__."/core/module_system/")) {
 
 $objDb = \Kajona\System\System\Carrier::getInstance()->getObjDB();
 
-$objDb->_pQuery("UPDATE kajona_system_config SET 'system_config_value' = 'false' WHERE system_config_name = '_admin_only_https_';", array());
-$objDb->_pQuery("UPDATE kajona_system_config SET 'system_config_value' = 'false' WHERE system_config_name = '_system_mod_rewrite_';", array());
-
+$objDb->_pQuery("UPDATE kajona_system_config SET system_config_value = 'false' WHERE system_config_name = '_admin_only_https_';", array());
+$objDb->_pQuery("UPDATE kajona_system_config SET system_config_value = 'false' WHERE system_config_name = '_system_mod_rewrite_';", array());
+$objDb->_pQuery("UPDATE kajona_user SET user_admin_language = 'de' WHERE user_username = 'test';", array());
