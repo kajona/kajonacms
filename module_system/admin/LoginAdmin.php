adb4670a108e4f5ea2a942eb1efaedc8ce792fc9
sidler <sidler@mulchprod.de>
2016-04-29 15:21:49 +0200

    BUG: system | LoginAdmin.php.php -> fixed a possible erroneous boolean expression found by AppChecker. Code not reachable in regular scenarios, so no impact on regular installations. fixed #98.

--- module_system/admin/LoginAdmin.php
+++ module_system/admin/LoginAdmin.php
@@ -40,7 +40,7 @@ class LoginAdmin extends AdminController implements AdminInterface
 
         parent::__construct();
 
-        if ($this->getAction() != "pwdReset" || $this->getAction() != "adminLogin" || $this->getAction() != "adminLogout") {
+        if($this->getAction() == "list") {
             $this->setAction("login");
         }
     }
