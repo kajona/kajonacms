1cca3d8b8de219299c23327d3abff99053c897e8
sidler <sidler@mulchprod.de>
2016-04-30 15:46:57 +0200

    UPDATE: system | Database.php -> DDL operations flush the internal table cache

--- module_system/system/OrmBase.php
+++ module_system/system/OrmBase.php
@@ -27,7 +27,7 @@ abstract class OrmBase
     protected static $objHandleLogicalDeletedGlobal = null;
 
     /**
-     * Flag to change the handling of deleted obejcts locally, so only for the current instance of the ORM
+     * Flag to change the handling of deleted objects locally, so only for the current instance of the ORM
      * mapper.
      *
      * @var OrmDeletedhandlingEnum
