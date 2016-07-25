34c3d7bc26fe87bcbfc96b0306f5cd8ba3aa9b5e
mrbashshell <bash@stefanbongartz.de>
2016-07-25 11:50:21 +0200

    UPDATE: _debbuging | optimized script output and added CSV export of results

--- _debugging/debug/dbperf.php
+++ _debugging/debug/dbperf.php
@@ -17,8 +17,13 @@ echo "| Kajona Debug Subsystem                                                  
 echo "|                                                                               |\n";
 echo "+-------------------------------------------------------------------------------+\n";
 
-echo "Creating 200 records with sortmanager...\n";
 
+$strCsvFile = _realpath_."/files/public/dbperf.csv";
+@unlink($strCsvFile);
+
+echo "\n-----------------------------------------------------";
+
+echo "\n\n<b>Step 1:</b> Creating 200 records with sortmanager...\n";
 $arrTimestampStart = gettimeofday();
 
 $objNaviTree = new NavigationTree();
@@ -35,16 +40,17 @@ for($intI = 0; $intI < 200; $intI++) {
 $arrTimestampEnde = gettimeofday();
 $intTimeUsed = (($arrTimestampEnde['sec'] * 1000000 + $arrTimestampEnde['usec'])
         - ($arrTimestampStart['sec'] * 1000000 + $arrTimestampStart['usec'])) / 1000000;
-echo  "\n\n<b>PHP-Time:</b>                              " . number_format($intTimeUsed, 6) . " sec \n";
-$arrTimestampStart = gettimeofday();
+echo  "<b>PHP-Time</b> of creating records with sortmanager:             " . number_format($intTimeUsed, 6) . " sec \n";
+file_put_contents($strCsvFile, "1) Create with sm;".number_format($intTimeUsed, 3, ',','.')."\r\n", FILE_APPEND | LOCK_EX);
 
-echo "Deletion of entries...\n";
+
+
+echo "\n\n<b>Step 2:</b> Deletion of entries...\n";
+$arrTimestampStart = gettimeofday();
 for($intI = 40; $intI <= 60; $intI++) {
     $arrRecords[$intI]->deleteObjectFromDatabase();
     unset($arrRecords[$intI]);
 }
-
-
 
 foreach($arrRecords as $objOnePoint) {
     $objOnePoint->deleteObjectFromDatabase();
@@ -52,17 +58,17 @@ foreach($arrRecords as $objOnePoint) {
 
 $objNaviTree->deleteObjectFromDatabase();
 
-
 $arrTimestampEnde = gettimeofday();
 $intTimeUsed = (($arrTimestampEnde['sec'] * 1000000 + $arrTimestampEnde['usec'])
         - ($arrTimestampStart['sec'] * 1000000 + $arrTimestampStart['usec'])) / 1000000;
-echo  "\n\n<b>PHP-Time:</b>                              " . number_format($intTimeUsed, 6) . " sec \n";
-$arrTimestampStart = gettimeofday();
+echo  "<b>PHP-Time</b> of deleting entries:                              " . number_format($intTimeUsed, 6) . " sec \n";
+file_put_contents($strCsvFile, "2) Delete;".number_format($intTimeUsed, 3, ',','.')."\r\n", FILE_APPEND | LOCK_EX);
 
 
 
-echo "\n\nCreating 200 records without sortmanager...\n";
+echo "\n-----------------------------------------------------";
 
+echo "\n\n<b>Step 3:</b> Creating 200 records without sortmanager...\n";
 $arrTimestampStart = gettimeofday();
 
 $objRootAspect = new SystemAspect();
@@ -79,10 +85,13 @@ for($intI = 0; $intI < 200; $intI++) {
 $arrTimestampEnde = gettimeofday();
 $intTimeUsed = (($arrTimestampEnde['sec'] * 1000000 + $arrTimestampEnde['usec'])
         - ($arrTimestampStart['sec'] * 1000000 + $arrTimestampStart['usec'])) / 1000000;
-echo  "\n\n<b>PHP-Time:</b>                              " . number_format($intTimeUsed, 6) . " sec \n";
-$arrTimestampStart = gettimeofday();
+echo  "<b>PHP-Time</b> of creating without sortmanager:               " . number_format($intTimeUsed, 6) . " sec \n";
+file_put_contents($strCsvFile, "3) Create without sm;".number_format($intTimeUsed, 3, ',','.')."\r\n", FILE_APPEND | LOCK_EX);
 
-echo "Deletion of entries...\n";
+
+
+echo "\n\n<b>Step 4:</b> Deletion of entries...\n";
+$arrTimestampStart = gettimeofday();
 for($intI = 40; $intI <= 60; $intI++) {
     $arrRecords[$intI]->deleteObjectFromDatabase();
     unset($arrRecords[$intI]);
@@ -97,13 +106,13 @@ $objRootAspect->deleteObjectFromDatabase();
 $arrTimestampEnde = gettimeofday();
 $intTimeUsed = (($arrTimestampEnde['sec'] * 1000000 + $arrTimestampEnde['usec'])
         - ($arrTimestampStart['sec'] * 1000000 + $arrTimestampStart['usec'])) / 1000000;
+echo  "<b>PHP-Time</b> deleting entries:                              " . number_format($intTimeUsed, 6) . " sec \n";
+file_put_contents($strCsvFile, "4) Delete;".number_format($intTimeUsed, 3, ',','.')."\r\n", FILE_APPEND | LOCK_EX);
 
+echo "\n-----------------------------------------------------";
 
-echo  "\n\n<b>PHP-Time:</b>                              " . number_format($intTimeUsed, 6) . " sec \n";
-echo  "<b>Queries db/cachesize/cached/fired:</b>     " . \Kajona\System\System\Carrier::getInstance()->getObjDB()->getNumber() . "/" .
-    \Kajona\System\System\Carrier::getInstance()->getObjDB()->getCacheSize() . "/" .
-    \Kajona\System\System\Carrier::getInstance()->getObjDB()->getNumberCache() . "/" .
-    (\Kajona\System\System\Carrier::getInstance()->getObjDB()->getNumber() - \Kajona\System\System\Carrier::getInstance()->getObjDB()->getNumberCache()) . "\n";
+echo "\n\nDownload <a href=\""._webpath_."/files/public/dbperf.csv\">CSV file with results</a>";
 
+echo "\n-----------------------------------------------------";
 
 
