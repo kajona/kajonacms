7d35d1e733aeb0c11272ad1b8e5f27bb35d07247
Christoph Kappestein <christoph.kappestein@artemeon.de>
2016-08-15 09:01:10 +0200

    UPDATE: module_statustransition | Statustransition > update copyright header

--- module_statustransition/system/StatustransitionActionInterface.php
+++ module_statustransition/system/StatustransitionActionInterface.php
@@ -1,6 +1,8 @@
 <?php
 /*"******************************************************************************************************
-*   (c) 2010-2016 ARTEMEON                                                                              *
+*   (c) 2004-2006 by MulchProductions, www.mulchprod.de                                                 *
+*   (c) 2007-2016 by Kajona, www.kajona.de                                                              *
+*       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
 ********************************************************************************************************/
 
 namespace Kajona\Statustransition\System;
