67abe2742aded6f00862cc65de1ee88f32ac82ad
Christoph Kappestein <christoph.kappestein@artemeon.de>
2016-08-19 13:51:10 +0200

    UPDATE: module_system | SystemChangelogRenderer > remveo formentry renderer

--- module_system/system/SystemChangelogRenderer.php
+++ module_system/system/SystemChangelogRenderer.php
@@ -205,12 +205,25 @@ class SystemChangelogRenderer
             case FormentryDatetime::class:
             case "date":
             case "datetime":
-                return FormentryRenderer::renderDate($strValue);
+                /*
+                $objField = new FormentryDate("", null);
+                $objField->setStrValue($strValue);
+                return $objField->getValueAsText();
+                */
+
+                return self::renderDate($strValue);
                 break;
 
             case FormentryDropdown::class:
             case "dropdown":
-                return FormentryRenderer::renderDropdown($strValue, $arrDDValues);
+                /*
+                $objField = new FormentryDropdown("", null);
+                $objField->setArrKeyValues($arrDDValues);
+                $objField->setStrValue($strValue);
+                return $objField->getValueAsText();
+                */
+
+                return self::renderDropdown($strValue, $arrDDValues);
                 break;
 
             case FormentryObjectlist::class:
@@ -219,11 +232,17 @@ class SystemChangelogRenderer
             case "objectlist":
             case "prozess":
             case "oe":
-                return FormentryRenderer::renderSystemIds($strValue);
+                /*
+                $objField = new FormentryObjectlist("", null);
+                $objField->setStrValue(explode(",", $strValue));
+                return $objField->getValueAsText();
+                */
+
+                return self::renderSystemIds($strValue);
                 break;
 
             default:
-                return FormentryRenderer::renderText($strValue);
+                return self::renderText($strValue);
         }
     }
 
@@ -256,5 +275,87 @@ class SystemChangelogRenderer
         }
 
         return self::$arrRenderer[$strClass]->getVersionValue($strProperty, $strValue);
+    }
+
+    /**
+     * @param mixed $objValue
+     * @return string
+     */
+    private static function renderDate($objValue)
+    {
+        if ($objValue instanceof Date) {
+            $objDate = $objValue;
+        } else {
+            if (empty($objValue)) {
+                return "";
+            }
+
+            $objDate = new Date($objValue);
+        }
+
+        return dateToString($objDate, false);
+    }
+
+    /**
+     * @param mixed $objValue
+     * @param string $strSeperator
+     * @return string
+     */
+    private static function renderSystemIds($objValue, $strSeperator = ", ")
+    {
+        $arrSystemIds = array();
+
+        if (!is_array($objValue)) {
+            if (validateSystemid($objValue)) {
+                $arrSystemIds = array($objValue);
+            } elseif (strpos($objValue, ",") !== false) {
+                $arrSystemIds = array_filter(explode(",", $objValue), function ($strSystemId) {
+                    return validateSystemid($strSystemId);
+                });
+            }
+        } elseif (is_array($objValue)) {
+            $arrSystemIds = array_filter(array_map(function($objValue){
+                if (is_string($objValue)) {
+                    return validateSystemid($objValue) ? $objValue : null;
+                } elseif ($objValue instanceof Model) {
+                    return $objValue->getSystemid();
+                } else {
+                    return null;
+                }
+            }, $objValue));
+        }
+
+        $arrNames = array();
+        foreach ($arrSystemIds as $strSystemId) {
+            $objObject = Objectfactory::getInstance()->getObject($strSystemId);
+            if ($objObject instanceof ModelInterface) {
+                $arrNames[] = $objObject->getStrDisplayName();
+            }
+        }
+
+        return implode($strSeperator, $arrNames);
+    }
+
+    /**
+     * @param mixed $objValue
+     * @param array $arrDDValues
+     * @return mixed
+     */
+    private static function renderDropdown($objValue, array $arrDDValues)
+    {
+        if (array_key_exists($objValue, $arrDDValues)) {
+            return $arrDDValues[$objValue];
+        }
+
+        return $objValue;
+    }
+
+    /**
+     * @param mixed $objValue
+     * @return string
+     */
+    private static function renderText($objValue)
+    {
+        return $objValue;
     }
 }
