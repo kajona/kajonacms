91c2a92f5ab4945892b9a98cbf129fce9bc6a1c5
smeyer <stefan.meyer1@yahoo.de>
2016-09-05 09:05:33 +0200

    NEW: system | FormgeneratorFactory -> removed method to clear the form cache

--- module_system/admin/AdminFormgeneratorFactory.php
+++ module_system/admin/AdminFormgeneratorFactory.php
@@ -104,12 +104,4 @@ class AdminFormgeneratorFactory {
     public static function getKeyByModel(ModelInterface $objInstance) {
         return get_class($objInstance) . $objInstance->getSystemid();
     }
-
-    /**
-     * Clears the form cache
-     */
-    public static function clearCache() {
-        self::$arrForms = array();
-    }
-
 }
