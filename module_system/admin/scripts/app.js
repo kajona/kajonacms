0f65e0be423a423eaede8520104340a4fef454f6
Christoph Kappestein <christoph.kappestein@artemeon.de>
2016-09-07 16:29:40 +0200

    UPDATE: module_system | app.js > add global dialogs, change provides json structure and add tags.js

--- module_system/admin/scripts/app.js
+++ module_system/admin/scripts/app.js
@@ -1,6 +1,14 @@
 
-require(['jquery', 'jqueryui', 'jquery-touchPunch', 'bootstrap'], function ($, jqueryui, touch, bootstrap) {
+require(['jquery', 'jqueryui', 'jquery-touchPunch', 'bootstrap', 'dialog'], function ($, jqueryui, touch, bootstrap, ModalDialog) {
 
-
+    // @TODO this is really bad since we write to the global scope
+    KAJONA = {};
+    KAJONA.admin = {};
+    KAJONA.admin.folderview = {};
+    KAJONA.admin.folderview.dialog = new ModalDialog('folderviewDialog', 0);
+    jsDialog_0 = new ModalDialog('jsDialog_0', 0);
+    jsDialog_1 = new ModalDialog('jsDialog_1', 1);
+    jsDialog_2 = new ModalDialog('jsDialog_2', 2);
+    jsDialog_3 = new ModalDialog('jsDialog_3', 3);
 
 });
