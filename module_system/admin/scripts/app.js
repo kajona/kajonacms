a5aa7c9bf9b877c64200a5a0c10fb8f74a0e017b
Christoph Kappestein <christoph.kappestein@artemeon.de>
2016-09-15 13:39:20 +0200

    UPDATE: module_v4skin | main.tpl > add loader and kajona for BC reasons

--- module_system/admin/scripts/app.js
+++ module_system/admin/scripts/app.js
@@ -10,49 +10,10 @@ require([
         ajax, forms, lists, messaging, workingIndicator) {
 
     // BC layer
-    if (typeof KAJONA == "undefined") {
-        KAJONA = {
-            util: {},
-            portal: {
-                lang: {}
-            },
-            admin: {
-                lang: {},
-                forms: {}
-            }
-        };
-    }
-
-    KAJONA.util = util;
-    KAJONA.util.desktopNotification = desktopNotification;
-
-    KAJONA.admin.loader = loader;
-
-    KAJONA.admin.folderview = folderview;
-
     KAJONA.admin.folderview.dialog = new Dialog('folderviewDialog', 0);
     jsDialog_0 = new Dialog('jsDialog_0', 0);
     jsDialog_1 = new Dialog('jsDialog_1', 1);
     jsDialog_2 = new Dialog('jsDialog_2', 2);
     jsDialog_3 = new Dialog('jsDialog_3', 3);
-
-    KAJONA.admin.tooltip = tooltip;
-    KAJONA.admin.switchLanguage = function(strLanguageToLoad) {
-        util.switchLanguage(strLanguageToLoad);
-    };
-    //KAJONA.admin.permissions = permissions;
-    KAJONA.admin.statusDisplay = statusDisplay;
-    //KAJONA.admin.systemtask = systemTask;
-    KAJONA.admin.ajax = ajax;
-    KAJONA.admin.forms = forms;
-    KAJONA.admin.lists = lists;
-    //KAJONA.admin.dashboardCalendar = dashboardCalendar;
-    KAJONA.admin.messaging = messaging;
-    KAJONA.admin.renderTocNavigation = messaging.render;
-    KAJONA.admin.WorkingIndicator = workingIndicator;
-    //KAJONA.admin.changelog = changelog;
-
-    // BC layer now we can fire the jquery document ready events
-    $.holdReady(false);
 
 });
