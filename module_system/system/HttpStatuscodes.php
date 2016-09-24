58e0545799e993ae2500aefcef54895d9909dd8c
sidler <sidler@mulchprod.de>
2016-09-24 21:39:10 +0200

    NEW: system | HttpStatuscodes.php -> updated the http version from 1.0 to 1.1, enabling conditional gets and stricter caching

--- module_system/system/HttpStatuscodes.php
+++ module_system/system/HttpStatuscodes.php
@@ -21,49 +21,49 @@ class HttpStatuscodes
     /**
      * Status code (200) indicating all went ok
      */
-    const SC_OK = "HTTP/1.0 200 OK";
+    const SC_OK = "HTTP/1.1 200 OK";
 
     /**
      * Status code (302) indicating a redirect request
      */
-    const SC_REDIRECT = "HTTP/1.0 302 Found";
+    const SC_REDIRECT = "HTTP/1.1 302 Found";
 
     /**
      * Status code (400) indicating a malformed request
      */
-    const SC_BADREQUEST = "HTTP/1.0 400 Bad Request";
+    const SC_BADREQUEST = "HTTP/1.1 400 Bad Request";
 
     /**
      * Status code (401) indicating authentication is possible but has failed or not yet been provided.
      *
      */
-    const SC_UNAUTHORIZED = "HTTP/1.0 401 Unauthorized";
+    const SC_UNAUTHORIZED = "HTTP/1.1 401 Unauthorized";
 
 
     /**
      * Status code (403) indicating the server understood the request but refused to fulfill it.
      *
      */
-    const SC_FORBIDDEN = "HTTP/1.0 403 Forbidden";
+    const SC_FORBIDDEN = "HTTP/1.1 403 Forbidden";
 
     /**
      * Status code (404) indicating that the requested resource is not available.
      *
      */
-    const SC_NOT_FOUND = "HTTP/1.0 404 Not Found";
+    const SC_NOT_FOUND = "HTTP/1.1 404 Not Found";
 
     /**
      * Status code (304) indicating that a conditional GET operation found that the resource was available and not modified.
      *
      */
-    const SC_NOT_MODIFIED = "HTTP/1.0 304 Not Modified";
+    const SC_NOT_MODIFIED = "HTTP/1.1 304 Not Modified";
 
 
     /**
      * Status code (500) indicating an error on the serverside. The request was ok but the server encountered an error.
      *
      */
-    const SC_INTERNAL_SERVER_ERROR = "HTTP/1.0 500 Internal Server Error";
+    const SC_INTERNAL_SERVER_ERROR = "HTTP/1.1 500 Internal Server Error";
 
 
 }
