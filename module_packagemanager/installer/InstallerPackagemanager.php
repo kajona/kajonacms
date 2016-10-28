b8ba603199582ecd1d98dffa6af0e4d4f796e44c
sidler <sidler@mulchprod.de>
2016-10-28 21:12:40 +0200

    UPDATE: packagemanagement | InstallerPackagemanager -> pushed the update sequence to 5.0.4

--- module_packagemanager/installer/InstallerPackagemanager.php
+++ module_packagemanager/installer/InstallerPackagemanager.php
@@ -97,7 +97,7 @@ class InstallerPackagemanager extends InstallerBase implements InstallerInterfac
         }
         
         $arrModule = SystemModule::getPlainModuleData($this->objMetadata->getStrTitle(), false);
-        if($arrModule["module_version"] == "5.0.1" || $arrModule["module_version"] == "5.0.2" || $arrModule["module_version"] == "5.0.3") {
+        if($arrModule["module_version"] == "5.0.1" || $arrModule["module_version"] == "5.0.2" || $arrModule["module_version"] == "5.0.3" || $arrModule["module_version"] == "5.0.4") {
             $strReturn .= "Updating to 5.1...\n";
             $this->updateModuleVersion($this->objMetadata->getStrTitle(), "5.1");
         }
