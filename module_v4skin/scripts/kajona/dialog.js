3fb22fafd0c9ab1045c234ce4ed063ba7f910f7c
Christoph Kappestein <christoph.kappestein@artemeon.de>
2016-11-29 11:27:54 +0100

    NEW: module_v4skin | dialog.js > add bootstrap as dependency

--- module_v4skin/scripts/kajona/dialog.js
+++ module_v4skin/scripts/kajona/dialog.js
@@ -1,5 +1,5 @@
 
-define(['jquery'], function ($) {
+define(['jquery', 'bootstrap'], function ($, bootstrap) {
 
     return function (strDialogId, intDialogType, bitDragging, bitResizing) {
         this.dialog = null;
