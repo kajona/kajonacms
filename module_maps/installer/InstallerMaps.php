ba4972e5b64fef0e44e2838bb8324cd43a96450b
sidler <sidler@mulchprod.de>
2017-01-09 10:12:06 +0100

    UPDATE: maps -> bumped to 5.1.1, added the possibility to enter a maps API Key

--- module_maps/installer/InstallerMaps.php
+++ module_maps/installer/InstallerMaps.php
@@ -69,6 +69,12 @@ class InstallerMaps extends InstallerBase implements InstallerInterface
             $this->updateElementAndModule("5.1");
         }
 
+        $arrModule = SystemModule::getPlainModuleData($this->objMetadata->getStrTitle(), false);
+        if ($arrModule["module_version"] == "5.1") {
+            $strReturn .= "Updating 5.1 to 5.1.1...\n";
+            $this->updateElementAndModule("5.1.1");
+        }
+
         return $strReturn;
     }
 
