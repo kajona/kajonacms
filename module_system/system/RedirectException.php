f4fca7fbe7aa2074445c5d91700191fffb7d6638
Christoph Kappestein <christoph.kappestein@artemeon.de>
2017-02-27 17:39:21 +0100

    UPDATE: module_system | RequestDispatcher > handle redirect exception

---
+++ module_system/system/RedirectException.php
@@ -0,0 +1,49 @@
+<?php
+/*"******************************************************************************************************
+*   (c) 2007-2016 by Kajona, www.kajona.de                                                              *
+*       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
+********************************************************************************************************/
+
+namespace Kajona\System\System;
+
+/**
+ * Exception to directly redirect the user to another url
+ *
+ * @author christoph.kappestein@artemeon.de
+ * @since 6.1
+ */
+class RedirectException extends Exception
+{
+    /**
+     * @var string
+     */
+    protected $strModule;
+
+    /**
+     * @var string
+     */
+    protected $strAction;
+
+    /**
+     * @var array
+     */
+    protected $arrParams;
+
+    /**
+     * @param string $strModule
+     * @param string $strAction
+     * @param array $arrParams
+     */
+    public function __construct($strModule, $strAction, array $arrParams)
+    {
+        parent::__construct(null, null);
+    }
+
+    /**
+     * @return string
+     */
+    public function getHref()
+    {
+        return Link::getLinkAdminHref($this->strModule, $this->strAction, $this->arrParams);
+    }
+}
