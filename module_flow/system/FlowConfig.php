d9351f7ba8a4f7e48888d820c13dc393d6d643c0
Christoph Kappestein <christoph.kappestein@artemeon.de>
2017-02-28 17:53:16 +0100

    UPDATE: module_flow | FlowConfig > add methods to validate a flow and automatically create start and end status

--- module_flow/system/FlowConfig.php
+++ module_flow/system/FlowConfig.php
@@ -8,6 +8,7 @@
 namespace Kajona\Flow\System;
 
 use Kajona\System\System\AdminListableInterface;
+use Kajona\System\System\Carrier;
 use Kajona\System\System\Model;
 use Kajona\System\System\ModelInterface;
 use Kajona\System\System\Pluginmanager;
@@ -52,6 +53,18 @@ class FlowConfig extends Model implements ModelInterface, AdminListableInterface
      * @var FlowStatus[]
      */
     private $arrStatus;
+
+    /**
+     * @var FlowManager
+     */
+    private $objFlowManager;
+
+    public function __construct($strSystemid = "")
+    {
+        parent::__construct($strSystemid);
+
+        $this->objFlowManager = Carrier::getInstance()->getContainer()->offsetGet(ServiceProvider::STR_MANAGER);
+    }
 
     /**
      * @return string
@@ -163,10 +176,91 @@ class FlowConfig extends Model implements ModelInterface, AdminListableInterface
     {
         if (!$this->objHandler) {
             $strClass = $this->getStrHandlerClass();
-            $this->objHandler = new $strClass();
+            $this->objHandler = new $strClass($this->objFlowManager);
         }
 
         return $this->objHandler;
+    }
+
+    public function validateFlow()
+    {
+        $arrStatus = $this->getArrStatus();
+
+        if (count($arrStatus) < 2) {
+            throw new \InvalidArgumentException("Flow must have at least two status");
+        }
+
+        // check start and end status must be 0 and 1
+        if ($arrStatus[0]->getIntIndex() !== 0) {
+            throw new \InvalidArgumentException("Start status must have the index 0");
+        }
+
+        if ($arrStatus[count($arrStatus) - 1]->getIntIndex() !== 1) {
+            throw new \InvalidArgumentException("End status must have the index 1");
+        }
+
+        // check whether there is a flow between the start and end status
+        $arrMatrix = $this->getDependencyMatrix();
+        $arrVisited = [];
+        if (!$this->findWayThroughFlow($arrMatrix, 0, 1, $arrVisited)) {
+            throw new \InvalidArgumentException("Inconsistent status flow");
+        }
+
+        // we have nodes which are not connected to the flow
+        if (count($arrVisited) != $arrStatus) {
+            throw new \InvalidArgumentException("Inconsistent status flow");
+        }
+    }
+
+    /**
+     * Recursive method to determine whether all status entries are connected
+     *
+     * @param array $arrMatrix
+     * @param int $intFromStatus
+     * @param int $intToStatus
+     * @param array $arrVisited
+     * @return bool
+     */
+    private function findWayThroughFlow(array $arrMatrix, $intFromStatus, $intToStatus, array &$arrVisited) : bool
+    {
+        if ($intFromStatus === $intToStatus) {
+            return true;
+        }
+
+        if (in_array($intFromStatus, $arrVisited)) {
+            return false;
+        }
+
+        if (isset($arrMatrix[$intFromStatus])) {
+            $arrVisited[] = $intFromStatus;
+
+            foreach ($arrMatrix[$intFromStatus] as $intStatus) {
+                if ($this->findWayThroughFlow($arrMatrix, $intStatus, $intToStatus, $arrVisited)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @return array
+     */
+    private function getDependencyMatrix() : array
+    {
+        $arrStatus = $this->getArrStatus();
+        $arrMatrix = [];
+        foreach ($arrStatus as $objStatus) {
+            $arrTransitions = $objStatus->getArrTransitions();
+            $arrTargets = [];
+            foreach ($arrTransitions as $objTransition) {
+                $arrTargets[] = $objTransition->getTargetStatus()->getIntIndex();
+            }
+            $arrMatrix[$objStatus->getIntIndex()] = $arrTargets;
+        }
+
+        return $arrMatrix;
     }
 
     /**
@@ -190,6 +284,21 @@ class FlowConfig extends Model implements ModelInterface, AdminListableInterface
                 $objFlow->setStrTargetClass($objHandler->getTargetClass());
                 $objFlow->setStrHandlerClass($strClass);
                 $objFlow->updateObjectToDb();
+
+                // we create automatically the start and end status
+                $objRedStatus = new FlowStatus();
+                $objRedStatus->setStrName("In Bearbeitung");
+                $objRedStatus->setStrIcon("icon_flag_red");
+                $objRedStatus->updateObjectToDb($objFlow->getSystemid());
+
+                $objGreenStatus = new FlowStatus();
+                $objGreenStatus->setStrName("Freigegeben");
+                $objGreenStatus->setStrIcon("icon_flag_green");
+                $objGreenStatus->updateObjectToDb($objFlow->getSystemid());
+
+                $objTransition = new FlowTransition();
+                $objTransition->setStrTargetStatus($objGreenStatus->getSystemid());
+                $objTransition->updateObjectToDb($objRedStatus->getSystemid());
             } else {
                 // @TODO maybe update
             }
@@ -203,8 +312,9 @@ class FlowConfig extends Model implements ModelInterface, AdminListableInterface
      */
     public static function getAvailableHandler()
     {
+        $objFlowManager = Carrier::getInstance()->getContainer()->offsetGet(ServiceProvider::STR_MANAGER);
         $objPluginManager = new Pluginmanager(FlowHandlerInterface::EXTENSION_POINT);
-        $arrPlugins = $objPluginManager->getPlugins();
+        $arrPlugins = $objPluginManager->getPlugins([$objFlowManager]);
 
         /** @var FlowHandlerInterface[] $arrHandler */
         $arrHandler = array();
