a1b1cbe2e51a9528ed5cc03890b0825ebc6c8e0c
Christoph Kappestein <christoph.kappestein@artemeon.de>
2017-03-01 14:01:14 +0100

    UPDATE: module_flow | FlowStatus > use only parentid as id generator key

--- module_flow/system/FlowStatus.php
+++ module_flow/system/FlowStatus.php
@@ -158,7 +158,7 @@ class FlowStatus extends Model implements ModelInterface, AdminListableInterface
     {
         // set index if we create a new record
         if (!validateSystemid($this->getSystemid())) {
-            $this->intIndex = IdGenerator::generateNextId(_flow_module_id_ . $this->getPrevId()) - 1;
+            $this->intIndex = IdGenerator::generateNextId($this->getPrevId()) - 1;
         }
 
         return parent::updateObjectToDb($strPrevId);
