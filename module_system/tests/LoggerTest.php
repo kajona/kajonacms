ff4ea9157be6dfbfd45c8edbe30d256672e02752
Christoph Kappestein <christoph.kappestein@artemeon.de>
2017-04-07 14:31:23 +0200

    NEW: module_system | LoggerTest > update logger test

--- module_system/tests/LoggerTest.php
+++ module_system/tests/LoggerTest.php
@@ -6,71 +6,33 @@ use Kajona\System\System\Carrier;
 use Kajona\System\System\Filesystem;
 use Kajona\System\System\Logger;
 use Kajona\System\System\StringUtil;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
 
 class LoggerTest extends Testbase
 {
-
     public function testLogger()
     {
-        
         $objLogger = Logger::getInstance("test.log");
+        $strFile = _realpath_ . _projectpath_ . "/log/test-" . date('Y-m-d') . ".log";
 
-        $objLogger->setIntLogLevel(Logger::$levelError);
-        $objLogger->addLogRow("test log row 1", Logger::$levelInfo);
+        file_put_contents($strFile, "");
 
-        $this->assertEquals($objLogger->getLogFileContent(), "");
+        $objLogger->debug("test log row debug");
+        $objLogger->info("test log row info");
+        $objLogger->notice("test log row notice");
+        $objLogger->warning("test log row warning");
+        $objLogger->error("test log row error");
+        $objLogger->critical("test log row critical");
+        $objLogger->alert("test log row alert");
+        $objLogger->emergency("test log row emergency");
+        $objLogger->log(LogLevel::INFO, "test log row emergency");
 
-        $objLogger->setIntLogLevel(Logger::$levelInfo);
-        $objLogger->addLogRow("test log row 1", Logger::$levelInfo);
+        $strContent = file_get_contents($strFile);
 
-        $this->assertFileExists(_realpath_ . _projectpath_ . "/log/test.log");
+        var_dump($strContent);
 
-        $this->assertTrue(StringUtil::indexOf($objLogger->getLogFileContent(), "test log row 1", false) !== false);
-
-
-        $objFilesystem = new Filesystem();
-        $objFilesystem->fileDelete(_projectpath_ . "/log/test.log");
-        $this->assertFileNotExists(_realpath_ . _projectpath_ . "/log/test.log");
-
+        $this->assertTrue(StringUtil::indexOf($strContent, "test log row 1", false) !== false);
     }
-
-    public function testNormalLogLevel()
-    {
-
-        $objLogger = Logger::getInstance('test_logger_normal.log');
-
-        $this->assertInstanceOf('Kajona\\System\\System\\Logger', $objLogger);
-        $this->assertEquals(2, $objLogger->getIntLogLevel());
-
-        $objLogger->addLogRow("test log row 3", Logger::$levelInfo);
-        $objLogger->addLogRow("test log row 2", Logger::$levelWarning);
-        $objLogger->addLogRow("test log row 1", Logger::$levelError);
-
-        $this->assertFileExists(_realpath_ . _projectpath_ . "/log/test_logger_normal.log");
-        $this->assertTrue(StringUtil::indexOf($objLogger->getLogFileContent(), 'test log row 3', false) === false);
-        $this->assertTrue(StringUtil::indexOf($objLogger->getLogFileContent(), 'test log row 2', false) !== false);
-        $this->assertTrue(StringUtil::indexOf($objLogger->getLogFileContent(), 'test log row 1', false) !== false);
-    }
-
-    public function testCustomLogLevel()
-    {
-
-        Carrier::getInstance()->getObjConfig()->setDebug('debuglogging_overwrite', array('test_logger_custom.log' => 1));
-
-        $objLogger = Logger::getInstance('test_logger_custom.log');
-
-        $this->assertInstanceOf('Kajona\\System\\System\\Logger', $objLogger);
-        $this->assertEquals(1, $objLogger->getIntLogLevel());
-
-        $objLogger->addLogRow("test log row 3", Logger::$levelInfo);
-        $objLogger->addLogRow("test log row 2", Logger::$levelWarning);
-        $objLogger->addLogRow("test log row 1", Logger::$levelError);
-
-        $this->assertFileExists(_realpath_ . _projectpath_ . "/log/test_logger_custom.log");
-        $this->assertTrue(StringUtil::indexOf($objLogger->getLogFileContent(), 'test log row 3', false) === false);
-        $this->assertTrue(StringUtil::indexOf($objLogger->getLogFileContent(), 'test log row 2', false) === false);
-        $this->assertTrue(StringUtil::indexOf($objLogger->getLogFileContent(), 'test log row 1', false) !== false);
-    }
-
 }
 
