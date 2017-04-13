2bb9ab0e0a053a655d98f98ca8efa4fd35e0a2b5
Stefan Idler <sidler@mulchprod.de>
2017-04-13 09:49:15 +0200

    NEW: system | PermissionsAnnotationTest.php -> added a testcase to validate permissions annotations for controller actions

---
+++ module_system/tests/PermissionsAnnotationTest.php
@@ -0,0 +1,73 @@
+<?php
+/*"******************************************************************************************************
+*   (c) 2015-2017 by Kajona, www.kajona.de                                                              *
+*       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
+********************************************************************************************************/
+
+namespace Kajona\System\Tests;
+
+use Kajona\System\System\AbstractController;
+use Kajona\System\System\Classloader;
+use Kajona\System\System\Reflection;
+use Kajona\System\System\Resourceloader;
+use Kajona\System\System\StringUtil;
+
+/**
+ * @author sidler@mulchprod.de
+ * @since 6.2
+ */
+class PermissionsAnnotationTest extends \PHPUnit_Framework_TestCase
+{
+    public function permissionDataProvider()
+    {
+        $arrReturn = [];
+
+        //load all admin-classes
+        $arrFilesBackend = Resourceloader::getInstance()->getFolderContent("/admin", [".php"], true, null,
+            function (&$strOneFile, $strPath) {
+                $strOneFile = Classloader::getInstance()->getInstanceFromFilename($strPath, AbstractController::class, null, [], true);
+            }
+        );
+
+
+        $arrFilesFrontend = Resourceloader::getInstance()->getFolderContent("/portal", [".php"], true, null,
+            function (&$strOneFile, $strPath) {
+                $strOneFile = Classloader::getInstance()->getInstanceFromFilename($strPath, AbstractController::class, null, [], true);
+            }
+        );
+
+        foreach (array_merge($arrFilesBackend, $arrFilesFrontend) as $objController) {
+            if ($objController == null) {
+                continue;
+            }
+
+            $objReflection = new \ReflectionClass($objController);
+
+
+            foreach ($objReflection->getMethods() as $objOneMethod) {
+                if (StringUtil::startsWith($objOneMethod->getName(), "action") && $objOneMethod->getName() !== "action") {
+                    $arrReturn[] = [$objReflection->getName(), $objOneMethod->getName()];
+                }
+            }
+
+        }
+
+        return $arrReturn;
+    }
+
+    /**
+     * @dataProvider permissionDataProvider
+     *
+     * @param $strClass
+     * @param $strMethod
+     */
+    public function testPermissionsAnnotation($strClass, $strMethod)
+    {
+        $this->markTestSkipped("Pending until Kajona 7.0");
+        $objKajonaReflection = new Reflection($strClass);
+        $strPermissions = $objKajonaReflection->getMethodAnnotationValue($strMethod, AbstractController::STR_PERMISSION_ANNOTATION);
+        $this->assertNotEmpty($strPermissions, $strClass."::".$strMethod);
+    }
+
+
+}
