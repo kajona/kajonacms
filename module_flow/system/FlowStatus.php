bf0a848ad92ae0d7bc8bcf6f96f0998d17ff4a9d
Christoph Kappestein <christoph.kappestein@artemeon.de>
2017-05-04 16:46:37 +0200

    UPDATE: module_flow | FlowStatus > use correct previd

--- module_flow/system/FlowStatus.php
+++ module_flow/system/FlowStatus.php
@@ -182,7 +182,7 @@ class FlowStatus extends Model implements ModelInterface, AdminListableInterface
     {
         // set index if we create a new record
         if (!validateSystemid($this->getSystemid())) {
-            $this->intIndex = IdGenerator::generateNextId($this->getPrevId()) - 1;
+            $this->intIndex = IdGenerator::generateNextId($strPrevId) - 1;
         }
 
         return parent::updateObjectToDb($strPrevId);
